30415ee56f097063d131bdde9ebcf26b55f27d25
GTD-Carthage <gtdcarthage@yahoo.com>
2018-07-09 20:53:27 +0800

    - Added new zigzag lift connector! - Increased the probablity of some old L-connector prefabs spawning.

---
+++ joiner/zigzag_lift.lua
@@ -0,0 +1,58 @@
+PREFABS.Joiner_zigzag_lift =
+{
+  file   = "joiner/zigzag_lift.wad"
+  map = "MAP01"
+
+  prob   = 1200
+  style  = "steepness"
+  theme  = "!hell"
+
+  env      = "!cave"
+  neighbor = "!cave"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 3
+  seed_h = 1
+
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+
+  delta_h  = 128
+  nearby_h = 128
+  can_flip = true
+
+}
+
+PREFABS.Joiner_zigzag_lift =
+{
+  file   = "joiner/zigzag_lift.wad"
+  map = "MAP02"
+
+  prob   = 1200
+  style  = "steepness"
+  theme  = "!hell"
+
+  env      = "!cave"
+  neighbor = "!cave"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 3
+  seed_h = 2
+
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+  y_fit  = "frame"
+
+  delta_h  = 128
+  nearby_h = 128
+  can_flip = true
+
+}
