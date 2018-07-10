9d48c9fc56b333c0ec1bacfe0af407efc0a425b9
GTD-Carthage <gtdcarthage@yahoo.com>
2018-07-10 11:45:21 +0800

    - Created a new hell-on-theme tentacle pillar made to look like it's breaking through floors and ceiling.

---
+++ decor/gtd_decor_hellontheme_tentaclepillar.lua
@@ -0,0 +1,16 @@
+PREFABS.Decor_tentaclepillar =
+{
+  file   = "decor/gtd_decor_hellontheme_tentaclepillar.wad"
+  map    = "MAP01"
+
+  prob   = 3000
+
+  where  = "point"
+  size   = 64
+  height = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit  = { 64-4,64+4 }
+}
