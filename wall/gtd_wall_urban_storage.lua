c3aa0dbcf879804a92d65612c1690bbaa1968531
GTD-Carthage <gtdcarthage@yahoo.com>
2018-07-14 11:46:08 +0800

    - Added new 'storage room' group prefabs for tech and urban - probably doesn't actually work. - Modified a lot of textures to have new texture alignments.

---
+++ wall/gtd_wall_urban_storage.lua
@@ -0,0 +1,75 @@
+PREFABS.Wall_urban_storage1 =
+{
+  file   = "wall/gtd_wall_urban_storage.wad"
+  map    = "MAP01"
+
+  prob   = 50
+  theme  = "!hell"
+  env = "building"
+
+  group = "gtd_wall_urban_storage"
+  
+  where  = "edge"
+  deep   = 16
+  height = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit  = "top"
+}
+
+PREFABS.Wall_urban_storage2 =
+{
+  template = "Wall_urban_storage1"
+
+  map = "MAP02"
+}
+
+PREFABS.Wall_urban_storage3 =
+{
+  template = "Wall_urban_storage1"
+
+  map = "MAP03"
+}
+
+PREFABS.Decor_urban_storage =
+{
+  file = "wall/gtd_wall_urban_storage"
+  map = "MAP04"
+  
+  prob = 50
+  theme = "!hell"
+  env = "building"
+  
+  group = "gtd_wall_urban_storage"
+
+  where = "point"
+  size = 64
+  
+  height = 128
+  
+  bound_z1 = 0
+  bound_z2 = 128
+  
+  z_fit = "top"
+}
+
+PREFABS.Wall_urban_storage_diag =
+{
+  file   = "wall/gtd_wall_urban_storage.wad"
+  map    = "MAP05"
+  
+  prob   = 50
+  
+  group = "gtd_wall_urban_storage"
+
+  where  = "diagonal"
+
+  height = 128
+  
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit  = "top"
+}
