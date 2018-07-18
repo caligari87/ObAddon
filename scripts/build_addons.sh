b06c94bc7b3c2dd22597a4e7147ed72f95dcae57
Sterling Parker <caligari.87@gmail.com>
2018-07-18 08:28:56 -0600

    updated test script

--- scripts/build_addons.sh
+++ scripts/build_addons.sh
@@ -3,9 +3,9 @@
 echo This file is not ready.
 exit 1
 
-mkdir build/files/games/doom/fabs
-cp ../src/prefabs build/files/games/doom/fabs
-zip build/ObFab.pk3 build/files/*
+mkdir -p build/files/games/doom/fabs
+cp -r ../src/prefabs/* build/files/games/doom/fabs
+#zip build/ObFab.pk3 build/files/*
 
 echo build complete
-# exit 0
+exit 0
