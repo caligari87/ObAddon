88304adcfa8edf03d7e329dfdbadfb0624999969
GTD-Carthage <gtdcarthage@yahoo.com>
2018-07-18 20:01:31 +0800

    Significantly altered directory structure to compensate for inclusion of games/doom/themes.lua, required by new group prefabs.

---
+++ prefabs/decor/gtd_decor.lua
@@ -0,0 +1,249 @@
+PREFABS.Decor_light_pillar_helix =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP01"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 128
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = { 120+16,120+32 }
+}
+
+PREFABS.Decor_light_pillar_helix_outdoor =
+{
+  template = "Decor_light_pillar_helix"
+
+  env    = "outdoor"
+
+  z_fit  = "top"
+}
+
+PREFABS.Decor_light_pillar_core =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP02"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = { 40+16,40+32}
+}
+
+PREFABS.Decor_light_pillar_core_outdoor =
+{
+  template = "Decor_light_pillar_core"
+
+  env    = "outdoor"
+
+  z_fit  = "top"
+}
+
+PREFABS.Decor_light_pillar_core2 =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP03"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = { 40+16,40+32 }
+}
+
+PREFABS.Decor_light_pillar_core2_outdoor =
+{
+  template = "Decor_light_pillar_core2"
+
+  env    = "outdoor"
+
+  z_fit  = "top"
+}
+
+PREFABS.Decor_data_pillar =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP04"
+
+  prob   = 5000
+  theme  = "tech"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = "top"
+}
+
+PREFABS.Decor_computer_tall =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP05"
+
+  prob   = 5000
+  theme  = "tech"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = "top"
+}
+
+PREFABS.Decor_server_rack =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP06"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+  
+  z_fit = "top"
+}
+
+PREFABS.Decor_open_pipe =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP07"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = "top"
+}
+
+PREFABS.Decor_floor_decal_stripes =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP08"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z1 = 2
+  
+  sink_mode = "never"
+}
+
+PREFABS.Decor_sealed_storage =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP09"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+  
+  z_fit = "top"
+}
+
+PREFABS.Decor_sealed_storage_outdoor =
+{
+  template = "Decor_sealed_storage"
+
+  env    = "outdoor"
+}
+
+PREFABS.Decor_sealed_storage_large =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP10"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "point"
+  size   = 128
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit = "top"
+}
+
+PREFABS.Decor_sealed_storage_large_outdoor =
+{
+  template = "Decor_sealed_storage_large"
+
+  env    = "outdoor"
+}
+
+PREFABS.Decor_fuel_rods =
+{
+  file   = "decor/gtd_decor.wad"
+  map    = "MAP11"
+
+  prob   = 5000
+  theme  = "tech"
+  env    = "building"
+
+  where  = "point"
+  size   = 64
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit = { 56+8,56+16 }
+}
---
+++ prefabs/wall/gtd_wall_urban.lua
@@ -0,0 +1,139 @@
+PREFABS.Wall_fake_door =
+{
+  file   = "wall/gtd_wall_urban.wad"
+  map    = "MAP01"
+
+  prob   = 35
+  theme = "urban"
+  env = "building"
+  
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+}
+
+PREFABS.Wall_fake_door2 =
+{
+  template = "Wall_fake_door"
+  
+  tex_DOOR3 = "DOOR1"
+}
+
+PREFABS.Wall_modern_piping =
+{
+  file   = "wall/gtd_wall_urban.wad"
+  map    = "MAP02"
+
+  prob   = 50
+  env   = "building"
+  theme = "urban"
+  
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+}
+
+PREFABS.Wall_modern_shutter =
+{
+  file   = "wall/gtd_wall_urban.wad"
+  map    = "MAP03"
+
+  prob   = 15
+  theme = "urban"
+  env = "building"
+  
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 16
+  over   = -32
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+}
+
+PREFABS.Wall_modern_shutter2 =
+{
+  template = "Wall_modern_shutter"
+  
+  tex_STEP4 = "STEP1"
+}
+
+PREFABS.Wall_modern_shutter3 =
+{
+  template = "Wall_modern_shutter"
+  
+  tex_STEP4 = "STEP2"
+}
+
+PREFABS.Wall_modern_shutter4 =
+{
+  template = "Wall_modern_shutter"
+  
+  tex_STEP4 = "STEP3"
+}
+
+PREFABS.Wall_modern_shutter5 =
+{
+  template = "Wall_modern_shutter"
+  
+  tex_STEP4 = "STEP5"
+}
+
+
+PREFABS.Wall_fake_warehouse_window =
+{
+  file   = "wall/gtd_wall_urban.wad"
+  map    = "MAP04"
+
+  prob   = 50
+  theme = "urban"
+  
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "bottom"
+}
+
+PREFABS.Wall_double_dark_windows =
+{
+  file   = "wall/gtd_wall_urban.wad"
+  map    = "MAP05"
+
+  prob   = 50
+  theme = "urban"
+  
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "bottom"
+}
