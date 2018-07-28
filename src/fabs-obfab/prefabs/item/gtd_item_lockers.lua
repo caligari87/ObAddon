876dc5f905e3f481b40577dcf266a2a3470dbf9d
GTD-Carthage <gtdcarthage@yahoo.com>
2018-07-28 18:52:53 +0800

    - Updated Garrett's overturned cross prefab. - Added Garrett's new 'wall of pain' diagonal walls for hell theme! - Added a new item locker doodad - press the switch to open the shutter and reveal its contents!

---
+++ src/fabs-obfab/prefabs/item/gtd_item_lockers.lua
@@ -0,0 +1,21 @@
+PREFABS.Item_locker_bin =
+{
+  file  = "item/gtd_item_lockers.wad"
+  map = "MAP01"
+  
+  prob = 50
+  theme = "!hell"
+
+  where = "point"
+  height = 128
+  size = 56
+  
+  bound_z1 = 0
+}
+
+PREFABS.Item_locker_bin_keyed =
+{
+  template = "Item_locker_bin"
+
+  item_kind = "key"
+}
