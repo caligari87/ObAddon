c17482d71ecb5c840b1957d7a1f59b84391d1d75
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-17 13:20:52 +0800

    - Added Glaice'x fix to teleporter linedefs in Garrett's tech pad being inverted. - Added Glaice's density and weapon preference edits in monsters.lua - Revised skybox picking code, still needs improvement.

--- src/games/doom/fabs/skybox/garrett_skybox_city.lua
+++ src/games/doom/fabs/skybox/garrett_skybox_city.lua
@@ -3,16 +3,12 @@ PREFABS.Skybox_garrett_city =
   file  = "skybox/garrett_skybox_city.wad"
   map   = "MAP01"
 
+  prob  = 50
+
+  kind  = "skybox"
+
   theme = "urban"
-
-  kind = "skybox"
-
-  seed_w = 3
-  seed_h = 3
 
   bound_z0 = -256
   bound_z1 = 256
-
-  prob  = 50
-  where = "seeds"
 }
