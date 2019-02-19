d109e83d51151b2be10a56cb9d267aa84bf2c16e
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-19 13:12:01 +0800

    - Added the simplest joiner fab ever. So simple you won't even realize it's a joiner.

---
+++ src/games/doom/fabs/joiner/gtd_simplest.lua
@@ -0,0 +1,21 @@
+PREFABS.Joiner_simplest =
+{
+  file   = "joiner/gtd_simplest.wad"
+  map    = "MAP01"
+
+  prob   = 150
+  
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 1
+  seed_h = 1
+
+  deep = 16
+  over = 16
+
+  bound_z1 = -16
+
+  x_fit = {60 , 68}
+  y_fit = "frame"
+}
