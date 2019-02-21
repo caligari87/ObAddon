362f30afe36f8e8a75a86d0458275a4f995587a5
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-21 13:16:21 +0800

    - Implemented the first crusher joiner ever!

---
+++ src/games/doom/fabs/joiner/gtd_crushers.lua
@@ -0,0 +1,27 @@
+PREFABS.Joiner_tech_triple_crusher =
+{
+  file   = "joiner/gtd_crushers.wad"
+  map    = "MAP01"
+
+  prob   = 1000
+  
+  style  = "trap"
+  
+  theme  = "!hell"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep = 16
+  over = 16
+
+  bound_z1 = -16
+  
+  height = 128
+
+  x_fit = "frame"
+  y_fit = "frame"
+}
