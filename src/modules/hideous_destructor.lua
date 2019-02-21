fa9dca8762f1ea81a35ce18cec09338b8e763cca
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-21 13:08:30 +0800

    - Added a new joiner - unfolding stairs! Not yet implemented! - Added UI for enabling Hideous Destructor fabs, no logic attached yet.

---
+++ src/modules/hideous_destructor.lua
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------
+--  MODULE: Hideous Destructor support module
+------------------------------------------------------------------------
+
+HD_SUPPORT = { }
+
+HD_SUPPORT.WALL_CHOICES =
+{
+  "enable",  _("Enable"),
+  "disable", _("Disable"),
+}
+
+function HD_SUPPORT.setup(self)
+  for name,opt in pairs(self.options) do
+    local value = self.options[name].value
+    PARAM[name] = value
+  end
+end
+
+----------------------------------------------------------------
+
+OB_MODULES["hd_support"] =
+{
+  label = _("Hideous Destructor Support")
+
+  side = "left"
+  priority = 75
+
+  game = "doomish"
+
+  hooks =
+  {
+    setup = HD_SUPPORT.setup
+  }
+
+  options =
+  {
+    hd_cover_walls =
+    {
+      name = "hd_cover_walls"
+      label=_("Cover Walls")
+      choices=HD_SUPPORT.WALL_CHOICES
+      tooltip = "Adds a some Hideous Destructor-specific fabs such as walls with cover to serve Hideous Destructor gameplay better."
+      default = "disable"
+    }
+  }
+}
