cd0ac8d6bdd88d44bf43d82b0ea6922319ec4f3d
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-21 13:14:04 +0800

    - Fully implemented dynamic rising stairs fab!

---
+++ src/games/doom/fabs/joiner/gtd_stair_action.lua
@@ -0,0 +1,27 @@
+PREFABS.Joiner_unfolding_stairs =
+{
+  file   = "joiner/gtd_stair_action.wad"
+  map    = "MAP01"
+
+  prob   = 1000
+  
+  style  = "steepness"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep = 16
+  over = 16
+
+  bound_z1 = -16
+  
+  height = 128
+  delta_h = 128
+  nearby_h = 128
+
+  x_fit = {60 , 68}
+  y_fit = "frame"
+}
