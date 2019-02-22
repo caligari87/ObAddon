2c3d4ccc34d1d3f17dc228c20faf3d123b7dcb78
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-22 21:59:28 +0800

    - Attempted to fix the issue with windows opening into simple_fence vistas... again

--- src/scripts/room.lua
+++ src/scripts/room.lua
@@ -1466,7 +1466,7 @@ function Room_border_up()
     -- scenic to room --
 
     if not A1.room and A2.room then
-      if A1.border_type == "simple_fence" then
+      if A1.border_type == "simple_fence" or A1.border_type == nil then
         Junction_make_wall(junc)
       end
     end
@@ -1479,7 +1479,7 @@ function Room_border_up()
         Junction_make_wall(junc)
 
       elseif not A1.is_outdoor and not A1.is_cave then
-        if A2.border_type != "simple_fence" then
+        if A2.border_type != "simple_fence" or A2.border_type == nil then
           Room_make_windows(A1, A2)
         end
         Junction_make_wall(junc)
