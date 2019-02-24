c72bce0880ee31576412163b6975e59cedb34f48
Frozsoul <41276432+Frozsoul2019@users.noreply.github.com>
2019-02-24 13:49:26 -0500

    Added various pond types
    Added nukage, blood and lava ponds
    Decreased spawn probability

--- src/games/doom/fabs/decor/frozsoul_097_ponds.lua
+++ src/games/doom/fabs/decor/frozsoul_097_ponds.lua
@@ -1,15 +1,15 @@
 --
--- Outdoor pond based on Oblige 0.97
+-- Outdoor ponds based on Oblige 0.97
 --
 
 
-PREFABS.Decor_frozsoul_097_pond1 =
+PREFABS.Decor_frozsoul_097_water_pond1 =
 {
   file   = "decor/frozsoul_097_ponds.wad"
   map    = "MAP01"
 
-  prob   = 500000
-  theme  = "!hell"
+  prob   = 50000	-- high probability is needed to have a reasonable chance of appearing
+  theme  = "!hell"  
   env    = "outdoor"
 
   where  = "point"
@@ -20,8 +20,49 @@ PREFABS.Decor_frozsoul_097_pond1 =
   sink_mode = "never"
 }
 
-PREFABS.Decor_frozsoul_097_pond2 =
+PREFABS.Decor_frozsoul_097_water_pond2 =
 {
   template = "Decor_frozsoul_097_pond1"
   map      = "MAP02"
 }
+
+PREFABS.Decor_frozsoul_097_nukage_pond1 =
+{
+  template = "Decor_frozsoul_097_pond1"
+  flat_FWATER1 = "NUKAGE1"
+}
+
+PREFABS.Decor_frozsoul_097_nukage_pond2 =
+{
+  template = "Decor_frozsoul_097_pond2"
+  flat_FWATER1 = "NUKAGE1"
+}
+
+PREFABS.Decor_frozsoul_097_blood_pond1 =
+{
+  template = "Decor_frozsoul_097_pond1"
+  theme = "!tech"
+  flat_FWATER1 = "BLOOD1"
+}
+
+PREFABS.Decor_frozsoul_097_blood_pond2 =
+{
+  template = "Decor_frozsoul_097_pond2"
+  theme = "!tech"
+  flat_FWATER1 = "BLOOD1"
+}
+
+PREFABS.Decor_frozsoul_097_lava_pond1 =
+{
+  template = "Decor_frozsoul_097_pond1"
+  theme = "hell"
+  flat_FWATER1 = "LAVA1"
+}
+
+PREFABS.Decor_frozsoul_097_lava_pond2 =
+{
+  template = "Decor_frozsoul_097_pond2"
+  theme = "hell"
+  flat_FWATER1 = "LAVA1"
+}
+
