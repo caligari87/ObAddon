3e5560c975d2edf96a8efa97d4f67910976f5e85
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-25 14:02:30 +0800

    - Implemented Glaice's wood-n-skull shed fab.

---
+++ src/games/doom/fabs/decor/glaice_wood_skulls.lua
@@ -0,0 +1,17 @@
+PREFABS.Decor_wood_skull_shed =
+{
+  file   = "decor/glaice_wood_skulls.wad"
+  map    = "MAP01"
+
+  prob   = 5000
+
+  where  = "point"
+  theme  = "!tech"
+  size   = 192
+  height = 256
+
+  bound_z1 = 0
+  bound_z2 = 224
+
+  z_fit  = { 16,224 }
+}
