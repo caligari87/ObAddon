b815dfab525433a8e045d827731d9ff1974c0d05
GTD-Carthage <gtdcarthage@yahoo.com>
2019-02-25 14:14:58 +0800

    Implemented Glaice's library bookshelf fabs!

---
+++ src/games/doom/fabs/decor/glaice_library.lua
@@ -0,0 +1,43 @@
+PREFABS.Decor_3x3_bookshelves_thin =
+{
+  file   = "decor/glaice_library.wad"
+  map    = "MAP01"
+
+  prob   = 5000
+
+  where  = "point"
+  theme  = "!tech"
+
+  env    = "indoor"
+
+  size   = 240
+  height = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  tex_PANBLACK =
+  {
+    PANBLACK = 50
+    PANBLUE  = 50
+    PANRED   = 50
+  }
+}
+
+PREFABS.Decor_3x3_bookshelves_thick
+{
+  template = "Decor_3x3_bookshelves_thin"
+
+  map      = "MAP02"
+
+  size     = 320
+}
+
+PREFABS.Decor_3x1_bookshelves =
+{
+  template = "Decor_3x3_bookshelves_thin"
+
+  map      = "MAP03"
+
+  size     = 224
+}
