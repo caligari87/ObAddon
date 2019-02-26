d03279717876957b1f567a59f83b142b4e3362f6
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-02-26 01:38:43 -0500

    Added trap variant to item lava alcove
    Added trap variant to item lava alcove.

--- src/games/doom/fabs/item/gtd_lava_pit_alcove.lua
+++ src/games/doom/fabs/item/gtd_lava_pit_alcove.lua
@@ -21,3 +21,25 @@ PREFABS.Item_lava_pit_alcove =
   x_fit = "frame"
   y_fit = "top"
 }
+
+-- Lava trap
+PREFABS.Item_lava_pit_alcove_trap =
+{
+  file   = "item/gtd_lava_pit_alcove.wad"
+  map    = "MAP03"
+
+  prob   = 80
+  theme  = "!hell"
+  style  = "traps"
+
+  where  = "seeds"
+  seed_w = 3
+  seed_h = 2
+
+  height = 192
+  deep =  16
+  over = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
