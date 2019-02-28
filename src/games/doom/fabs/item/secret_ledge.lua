39f0dac353fd5d209cf572966f9f53e78c3f0224
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-02-28 00:01:10 -0500

    Added descending variant to secret_ledge.wad
    Added descending variant to secret_ledge.wad. May need X/Y tweaking in LUA.

--- src/games/doom/fabs/item/secret_ledge.lua
+++ src/games/doom/fabs/item/secret_ledge.lua
@@ -25,7 +25,6 @@ PREFABS.Item_secret_ledge =
 
   flat_CEIL3_1 = { CEIL3_1=50, CEIL3_2=50, FLOOR0_1=50, FLAT5_5=50, FLAT5=50, FLOOR0_2=50, FLOOR4_6=50, FLOOR5_3=50 }
   tex_STEP2 = { STEP2=50, STEP6=50 }
-  tex_METAL2 = { METAL2=50, TEKWALL1=50, TEKWALL4=50, BROWN144=50 }
 
   -- use the occasional-blink FX (fairly rarely)
   sector_1  = { [0]=15, [1]=50, [2]=10, [3]=10, [8]=50 }
@@ -38,4 +37,23 @@ PREFABS.Item_secret_ledge2 =
   theme = "tech"
 
   tex_SUPPORT3 = "DOORSTOP"
+}
+
+-- Variant goes down, with lift and platform to lower
+PREFABS.Item_secret_ledge2_down =
+{
+  template  = "Item_secret_ledge"
+  map = "MAP03"
+  theme "!tech"
+  prob = 800
+
+}
+
+PREFABS.Item_secret_ledge2_down_tech =
+{
+  template  = "Item_secret_ledge"
+  map = "MAP03"
+  theme = "tech"
+  prob = 800
+
 }
