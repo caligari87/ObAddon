48b0165145d9a3b5dc42b30599af26a73993b887
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-01 20:02:42 +0800

    - Added a new staircase - a 16-height sidewalk piece! You'll never even realize it's there...

---
+++ src/games/doom/fabs/stairs/sidewalk_16.lua
@@ -0,0 +1,20 @@
+PREFABS.Sidewalk_16 =
+{
+  file   = "stairs/sidewalk_16.wad"
+
+  prob   = 80
+  map    = "MAP01"
+
+  where  = "seeds"
+
+  seed_h = 1
+  seed_w = 1
+
+  x_fit  = "stretch"
+  y_fit  = "stretch"
+
+  bound_z1 = 0
+  bound_z2 = 16
+
+  delta_h = 16
+}
