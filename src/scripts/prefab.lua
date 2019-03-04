e91d4b97414d117f082fd0941c114e57f47edb94
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-04 23:22:46 +0800

    - Updated ZDoom Special Addons - successfully experimented with applying custom textures from Glaice's Oblige 6.2 Epic Texture Set.

--- src/scripts/prefab.lua
+++ src/scripts/prefab.lua
@@ -2,9 +2,10 @@
 --  WAD PREFAB SYSTEM
 ------------------------------------------------------------------------
 --
---  Oblige Level Maker
+--  Oblige Level Maker // ObAddon
 --
 --  Copyright (C) 2013-2017 Andrew Apted
+--  Copyright (C) 2019 MsrSgtShooterPerson
 --
 --  This program is free software; you can redistribute it and/or
 --  modify it under the terms of the GNU General Public License
