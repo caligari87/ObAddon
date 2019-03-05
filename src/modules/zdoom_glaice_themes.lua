6424ae43df7748758087e9d4c115de74e3fcda07
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-05 09:50:02 +0800

    - Expanded ZDoom Special Addons to separate material and room themes into other Lua's for cleanliness.

---
+++ src/modules/zdoom_glaice_themes.lua
@@ -0,0 +1,56 @@
+GLAICE.THEMES =
+{
+  tech_GrayMet =
+  {
+    env = "building"
+    prob = 50
+
+    walls =
+    {
+      GRAYMET1 = 30
+      GRAYMET2 = 30
+      GRAYMET3 = 30
+      GRAYMET4 = 30
+      GRAYMET5 = 30
+    }
+
+    floors =
+    {
+      FLOOR4_8 = 50
+      FLOOR5_1 = 50
+      FLOOR5_3 = 30
+      FLOOR0_3 = 30
+      FLOOR5_1 = 25
+      FLOOR3_3 = 20
+      FLOOR0_2 = 20
+      FLOOR0_1 = 20
+      FLOOR4_6 = 15
+      FLOOR7_1 = 15
+      FLAT4 = 15
+      FLAT14 = 10
+      SLIME15 = 10
+      SLIME16 = 10
+      FLOOR1_6 = 6
+      FLOOR1_1 = 8
+      FLOOR0_5 = 5
+      FLAT5 = 5
+    }
+
+    ceilings =
+    {
+      CEIL3_1 = 20
+      CEIL3_2 = 20
+      CEIL3_5 = 20
+      CEIL3_3 = 15
+      CEIL4_2 = 10
+      CEIL4_3 = 10
+      CEIL5_1 = 15
+
+      FLAT9  = 30
+      FLAT19 = 20
+      FLAT4  = 20
+      FLAT9  = 15
+      FLAT23 = 5
+    }
+  }
+}
