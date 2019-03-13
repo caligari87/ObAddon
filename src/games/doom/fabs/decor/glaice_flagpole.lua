f4ba2677922ac387cec8bf2b4f1076a1b103cfe5
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-13 11:58:23 +0800

    - Added lua for Glaice's new flagpole decor fab!

---
+++ src/games/doom/fabs/decor/glaice_flagpole.lua
@@ -0,0 +1,19 @@
+PREFABS.Decor_glaice_flagpole =
+{
+  file   = "decor/glaice_flagpole.wad"
+  map    = "MAP01"
+
+  theme  = "!hell"
+  env = "outdoor"
+
+  prob   = 50
+
+  where  = "point"
+  height = 264
+  size   = 56
+
+  bound_z1 = 0
+  bound_z2 = 264
+
+  sink_mode = "never"
+}
