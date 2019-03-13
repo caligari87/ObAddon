28d93e716ae84dfb738a7acbb99f1f6349fcf422
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-13 17:57:50 +0800

    - Added Epic Texture variations of the basic Oblige ceiling lights.

---
+++ src/games/doom/fabs/decor/EPIC_ceil_light.lua
@@ -0,0 +1,657 @@
+--
+-- Simple ceiling light
+--
+
+PREFABS.Light_EPIC_basic =
+{
+  file   = "decor/ceil_light.wad"
+  map    = "MAP02"
+
+  prob   = 0
+  env    = "building"
+
+  uses_epic_textures = true
+
+  kind   = "light"
+  where  = "point"
+
+  height = 96
+
+  bound_z1 = -32
+  bound_z2 = 0
+
+  -- use the occasional-blink FX (fairly rarely)
+  sector_1  = { [0]=90, [1]=15 }
+
+}
+
+
+----------- URBAN THEME ------------------------
+
+
+PREFABS.Light_urban_LITBL3F1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITBL3F1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITBL3F2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITBL3F2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITE4F1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITE4F1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITE4F2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITE4F2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITES01 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITES01"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITES02 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITES02"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITES03 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITES03"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LITES04 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LITES04"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LIGHTS1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LIGHTS1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LIGHTS2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LIGHTS2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LIGHTS3 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LIGHTS3"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_LIGHTS4 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "LIGHTS4"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE5_1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE5_1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE5_2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE5_2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE5_3 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE5_3"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE65B =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE65B"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE65G =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE65G"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE65O =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE65O"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE65W =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE65W"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_urban_TLITE65Y =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "urban"
+
+  flat_TLITE6_4 = "TLITE65Y"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+
+----------- TECH THEME ------------------------
+
+
+PREFABS.Light_tech_LITBL3F1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITBL3F1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITBL3F2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITBL3F2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITE4F1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITE4F1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITE4F2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITE4F2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITES01 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITES01"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITES02 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITES02"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITES03 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITES03"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LITES04 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LITES04"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LIGHTS1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LIGHTS1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LIGHTS2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LIGHTS2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LIGHTS3 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LIGHTS3"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_LIGHTS4 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "LIGHTS4"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE5_1 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE5_1"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE5_2 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE5_2"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE5_3 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE5_3"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE65B =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE65B"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE65G =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE65G"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE65O =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE65O"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE65W =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE65W"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+PREFABS.Light_tech_TLITE65Y =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP02"
+
+  prob   = 50
+  theme  = "tech"
+
+  flat_TLITE6_4 = "TLITE65Y"
+
+  sector_1  = { [0]=90, [1]=15 }
+}
+
+
+----------- HELL THEME ------------------------
+
+
+PREFABS.Light_hell_GLITE01 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE01"
+}
+
+PREFABS.Light_hell_GLITE02 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE02"
+}
+
+PREFABS.Light_hell_GLITE03 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE03"
+}
+
+PREFABS.Light_hell_GLITE04 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE04"
+}
+
+PREFABS.Light_hell_GLITE05 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE05"
+}
+
+PREFABS.Light_hell_GLITE06 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE06"
+}
+
+PREFABS.Light_hell_GLITE07 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE07"
+}
+
+PREFABS.Light_hell_GLITE08 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE08"
+}
+
+PREFABS.Light_hell_GLITE09 =
+{
+  template = "Light_EPIC_basic"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "hell"
+
+  flat_TLITE6_4 = "GLITE09"
+}
