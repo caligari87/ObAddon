f182a7ac15e766ce37c3dc9795da35987db7199a
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-03-13 01:49:51 -0400

    Fixed MAP01 as intended and added banner on MAP02
    Needs some code for MAP02 since the texture for the banner are in the Epic Textures map set, hence the LUA they are set for "Unfinished".

--- src/games/doom/fabs/decor/glaice_flagpole.lua
+++ src/games/doom/fabs/decor/glaice_flagpole.lua
@@ -17,3 +17,47 @@ PREFABS.Decor_glaice_flagpole =
 
   sink_mode = "never"
 }
+
+-- Uses OBLIGE logo!!
+UNFINISHED.Decor_glaice_flagpole_obaddon_logo_earth =
+{
+  template = "Decor_glaice_flagpole"
+  map    = "MAP02"
+
+  theme  = "!hell"
+  env = "outdoor"
+
+  prob   = 80
+
+  where  = "point"
+  height = 280
+  size   = 56
+
+  bound_z1 = 0
+  bound_z2 = 264
+
+  sink_mode = "never"
+}
+
+-- Uses OBLIGE logo!!
+UNFINISHED.Decor_glaice_flagpole_obaddon_logo_hell =
+{
+  template = "Decor_glaice_flagpole"
+  map    = "MAP02"
+
+  theme  = "hell"
+  env = "outdoor"
+
+  prob   = 80
+
+  where  = "point"
+  height = 282
+  size   = 56
+
+  bound_z1 = 0
+  bound_z2 = 264
+
+  sink_mode = "never"
+
+  tex_OBDNBNR1 = "OBDNBNR2"
+}
