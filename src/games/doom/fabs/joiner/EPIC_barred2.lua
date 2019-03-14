75f2659aadc121872dc0ec84692412d7b6b9cbaa
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-14 16:21:06 +0800

    - Added epic texture variations of various door and door joiners.

---
+++ src/games/doom/fabs/joiner/EPIC_barred2.lua
@@ -0,0 +1,98 @@
+PREFABS.Joiner_EPIC_remote_door_tech =
+{
+  file   = "joiner/barred2.wad"
+  where  = "seeds"
+  shape  = "I"
+  map    = "MAP01"
+
+  theme = "tech"
+
+  uses_epic_textures = true
+
+  key    = "barred"
+
+  prob   = 40 * 7
+
+  seed_w = 2
+  seed_h = 1
+
+  deep   = 16
+  over   = 16
+
+  tag_1  = "?door_tag"
+  door_action = "S1_OpenDoor"
+
+  flat_FLAT1 =
+  {
+    FLAT1 = 50
+    FLOOR0_1 = 50
+    FLAT19 = 50
+  }
+
+  tex_SILVER3 =
+  {
+    BIGDOORA = 50
+    BIGDOORC = 50
+    BIGDOORD = 50
+    BIGDOORF = 50
+    BIGDOORG = 50
+    BIGDOORH = 50
+    BIGDOORL = 50
+  }
+
+  tex_FLAT23 =
+  {
+    CEIL5_1 = 50
+    CEIL5_2 = 50
+  }
+}
+
+PREFABS.Joiner_EPIC_remote_door_urban =
+{
+  template = "Joiner_EPIC_remote_door_tech"
+
+  theme = "urban"
+
+  flat_FLAT1 = { FLAT5_1=50, FLAT5_2=50 }
+
+  tex_SILVER3 =
+  {
+    BIGDOOR8 = 50
+    BIGDOOR9 = 50
+    BIGDOOR0 = 50
+    BIGDOORA = 50
+    BIGDOORB = 50
+    BIGDOORC = 50
+    BIGDOORD = 50
+    BIGDOORE = 50
+    BIGDOORF = 50
+    BIGDOORG = 50
+    BIGDOORH = 50
+    BIGDOORI = 50
+    BIGDOORJ = 50
+    BIGDOORK = 50
+    BIGDOORL = 50
+  }
+}
+
+PREFABS.Joiner_EPIC_remote_door_hell =
+{
+  template = "Joiner_EPIC_remote_door_tech"
+
+  theme = "hell"
+
+  flat_FLAT1 = { FLAT10=50, RROCK09=50, RROCK16=50 }
+
+  tex_SILVER3 =
+  {
+    BIGDOOR8 = 50
+    BIGDOOR9 = 50
+    BIGDOOR0 = 50
+    BIGDOORA = 50
+    BIGDOORB = 50
+    BIGDOORE = 50
+    BIGDOORI = 50
+    BIGDOORJ = 50
+    BIGDOORK = 50
+  }
+}
