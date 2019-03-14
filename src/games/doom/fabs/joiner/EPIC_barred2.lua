7225765358811bb5d71ff76c5eb02f13c1d08303
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-14 16:32:03 +0800

    - Added more EPIC texture variations to some switch-locked door fabs.

--- src/games/doom/fabs/joiner/EPIC_barred2.lua
+++ src/games/doom/fabs/joiner/EPIC_barred2.lua
@@ -96,3 +96,91 @@ PREFABS.Joiner_EPIC_remote_door_hell =
     BIGDOORK = 50
   }
 }
+
+PREFABS.Joiner_EPIC_remote_sw_metal_tech =
+{
+  template = "Joiner_EPIC_remote_door_tech"
+  key = "sw_metal"
+
+  prob = 15 * 7
+
+  theme = "tech"
+
+  x_fit = "frame"
+  y_fit = "stretch"
+
+  tex_SILVER3 =
+  {
+    BIGDOORA = 50
+    BIGDOORC = 50
+    BIGDOORD = 50
+    BIGDOORF = 50
+    BIGDOORG = 50
+    BIGDOORH = 50
+    BIGDOORL = 50
+  }
+
+  flat_FLAT23 = "CEIL5_2"
+}
+
+PREFABS.Joiner_EPIC_remote_sw_metal_urban =
+{
+  template = "Joiner_EPIC_remote_door_tech"
+  key = "sw_metal"
+
+  prob = 15 * 7
+
+  theme = "urban"
+
+  x_fit = "frame"
+  y_fit = "stretch"
+
+  tex_SILVER3 =
+  {
+    BIGDOOR8 = 50
+    BIGDOOR9 = 50
+    BIGDOOR0 = 50
+    BIGDOORA = 50
+    BIGDOORB = 50
+    BIGDOORC = 50
+    BIGDOORD = 50
+    BIGDOORE = 50
+    BIGDOORF = 50
+    BIGDOORG = 50
+    BIGDOORH = 50
+    BIGDOORI = 50
+    BIGDOORJ = 50
+    BIGDOORK = 50
+    BIGDOORL = 50
+  }
+
+  flat_FLAT23 = "CEIL5_2"
+}
+
+PREFABS.Joiner_EPIC_remote_sw_metal_hell =
+{
+  template = "Joiner_EPIC_remote_door_tech"
+  key = "sw_metal"
+
+  prob = 15 * 7
+
+  theme = "hell"
+
+  x_fit = "frame"
+  y_fit = "stretch"
+
+  tex_SILVER3 =
+  {
+    BIGDOOR8 = 50
+    BIGDOOR9 = 50
+    BIGDOOR0 = 50
+    BIGDOORA = 50
+    BIGDOORB = 50
+    BIGDOORE = 50
+    BIGDOORI = 50
+    BIGDOORJ = 50
+    BIGDOORK = 50
+  }
+
+  flat_FLAT23 = "CEIL5_2"
+}
