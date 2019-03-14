f7a30bac6b55909ccff6bd329e615790e2b78248
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-14 15:15:30 +0800

    - Added new storage lift platform deco fab.

---
+++ src/games/doom/fabs/decor/gtd_decor_storage_elevator.lua
@@ -0,0 +1,21 @@
+PREFABS.Decor_tech_point_HVAC_1 =
+{
+  file   = "decor/gtd_decor_storage_elevator.wad"
+  map    = "MAP01"
+
+  where  = "point"
+
+  prob   = 5000
+  theme  = "tech"
+  env    = "building"
+
+  size   = 136
+  height = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+
+  z_fit = { 68+4,96-4 }
+}
