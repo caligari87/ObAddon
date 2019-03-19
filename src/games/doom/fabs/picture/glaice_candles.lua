82ff396c1633c893414baf5b739dba5cd98ff233
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-19 10:52:31 +0800

    - Added Lua definition for the new candles pic prefab

---
+++ src/games/doom/fabs/picture/glaice_candles.lua
@@ -0,0 +1,23 @@
+PREFABS.Pic_glaice_candles =
+{
+  file   = "picture/glaice_candles.wad"
+  map    = "MAP01"
+
+  prob   = 15
+  theme = "hell"
+
+  where  = "seeds"
+  height = 128
+
+  seed_w = 1
+  seed_h = 1
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  deep   =  16
+  over   = -16
+
+  x_fit = { 56,72 }
+  y_fit = "top"
+}
