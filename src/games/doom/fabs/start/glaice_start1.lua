ab11fdb2de6464a1e98e59a1bb6f2a86433f19b4
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-28 13:36:35 +0800

    - Added lua for Glaice's new start fabs!

---
+++ src/games/doom/fabs/start/glaice_start1.lua
@@ -0,0 +1,39 @@
+PREFABS.Start_garrett_start_center_pillar =
+{
+  file  = "start/glaice_start1.wad"
+  map = "MAP01"
+
+  prob  = 50
+  theme = "!hell"
+
+  where = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep  =  16
+  over  = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
+
+PREFABS.Start_garrett_start_lift =
+{
+  file  = "start/glaice_start1.wad"
+  map = "MAP02"
+
+  prob  = 50
+  theme = "!hell"
+
+  where = "seeds"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep  =  16
+  over  = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
