0850f726455eb3c7a93bb422a740a06e0aebba3a
GTD-Carthage <gtdcarthage@yahoo.com>
2019-03-29 11:42:46 +0800

    - Added lua defs for Glaice's new exit fabs.

---
+++ src/games/doom/fabs/exit/glaice_simple_exit.lua
@@ -0,0 +1,18 @@
+PREFABS.Exit_glaice_simple_exit =
+{
+  file   = "exit/glaice_simple_exit.wad"
+  map    = "MAP01"
+
+  prob   = 300
+
+  where  = "seeds"
+
+  seed_w = 1
+  seed_h = 2
+
+  deep   =  16
+  over   = -16
+
+  x_fit  = "frame"
+  y_fit  = "top"
+}
---
+++ src/games/doom/fabs/exit/glaice_simple_exit2.lua
@@ -0,0 +1,18 @@
+PREFABS.Exit_glaice_simple_exit2 =
+{
+  file   = "exit/glaice_simple_exit2.wad"
+  map    = "MAP01"
+
+  prob   = 300
+
+  where  = "seeds"
+
+  seed_w = 1
+  seed_h = 1
+
+  deep   =  16
+  over   = -16
+
+  x_fit  = "frame"
+  y_fit  = "top"
+}
