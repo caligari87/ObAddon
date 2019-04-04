9303458ff2bf853b19d787dbf61d7dd84a23ff56
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-04 13:13:15 +0800

    - Added lua def for Glaice's crucified point decor.

---
+++ src/games/doom/fabs/decor/glaice_crucified.lua
@@ -0,0 +1,15 @@
+PREFABS.Decor_glaice_crufiied_pillar_1 =
+{
+  file   = "decor/glaice_crucified.wad"
+  map    = "MAP01"
+
+  prob   = 5000
+  theme  = "hell"
+  
+  where  = "point"
+  size   = 88
+  height = 136
+
+  bound_z1 = 0
+  bound_z2 = 138
+}
