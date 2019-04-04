bf4e68f1f3f983af89ff04fbc3184f1d6517535b
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-04-04 01:27:32 -0400

    Updated to include epic variant and others

--- src/games/doom/fabs/decor/glaice_crucified.lua
+++ src/games/doom/fabs/decor/glaice_crucified.lua
@@ -12,4 +12,17 @@ PREFABS.Decor_glaice_crufiied_pillar_1 =
 
   bound_z1 = 0
   bound_z2 = 138
+
+  tex_SP_DUDE4 = { SP_DUDE4=50, SP_DUDE5=50 }
+}
+
+PREFABS.Decor_glaice_crufiied_pillar_1_EPIC =
+{
+  template   = "Decor_glaice_crufiied_pillar_1"
+  prob       = 5000
+  
+  uses_epic_textures = true
+  replaces   = "Decor_glaice_crufiied_pillar_1"
+
+  tex_SP_DUDE4 = { SP_DUDE4=50, SP_DUDE5=50, SPDUDE3=50, SPDUDE6=30 }
 }
