b1587926f5c66e0b482d3b08a9428de54092f58a
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-04-08 20:02:41 -0400

    Alternate versions for two Doom1 wall prefabs
    Because certain blood things (blood and skin, bloody brain) don't exist in the game.

--- src/games/doom/fabs/wall/gtd_wall_infestation.lua
+++ src/games/doom/fabs/wall/gtd_wall_infestation.lua
@@ -9,6 +9,7 @@ PREFABS.Wall_infestation_guts_on_wall =
 
   prob   = 1.5
   env   = "building"
+  game  = "doom2"
 
   on_liquids = "never"
 
@@ -24,10 +25,18 @@ PREFABS.Wall_infestation_guts_on_wall =
   z_fit = "top"
 }
 
+PREFABS.Wall_infestation_guts_on_wall_DOOM1 =
+{
+  template   = "Wall_infestation_guts_on_wall"
+  game  = "doom"
+  thing_81 = "gibs"
+}
+
 PREFABS.Wall_infestation_guts_flowing_down_wall =
 {
   file   = "wall/gtd_wall_infestation.wad"
   map    = "MAP02"
+  game   = "doom2"
 
   prob   = 1.5
   env   = "building"
@@ -45,6 +54,13 @@ PREFABS.Wall_infestation_guts_flowing_down_wall =
   x_fit = "frame"
   z_fit = "top"
 }
+
+PREFABS.Wall_infestation_guts_flowing_down_wall_DOOM1 =
+{
+  template   = "Wall_infestation_guts_flowing_down_wall"
+  game       = "doom"
+  thing_79 = "gibs"
+ }
 
 PREFABS.Wall_infestation_evil_shrine_eye =
 {
