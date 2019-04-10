5c32dbae725eeddfc049110c38cb862c400be168
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-04-10 14:20:46 -0400

    Added additional copyrights on top

--- src/modules/zdoom_stories.lua
+++ src/modules/zdoom_stories.lua
@@ -3,6 +3,8 @@
 ----------------------------------------------------------------
 --
 --  Copyright (C) 2019 MsrSgtShooterPerson
+--  Copyright (C) 2019 Glaice
+--  Copyright (C) 2019 Tapwave
 --
 --  This program is free software; you can redistribute it and/or
 --  modify it under the terms of the GNU General Public License
