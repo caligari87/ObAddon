46ae49da6098fdc2a227d4d2b3bae0958334b921
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-16 15:32:58 +0800

    - Created a new toilet closet fab for weapons and powerups. You can't have enough toilet fabs!

---
+++ src/games/doom/fabs/item/gtd_item_closet_toilet_small.lua
@@ -0,0 +1,26 @@
+--
+-- Yes, I am aware I am a bad person.
+--
+
+PREFABS.Item_closet_toilet_room_small =
+{
+  file   = "item/gtd_item_closet_toilet_small.wad"
+  map    = "MAP01"
+
+  rank   = 2
+  prob   = 150
+  theme  = "!hell"
+  env    = "!cave"
+
+  where  = "seeds"
+  seed_w = 2
+  seed_h = 2
+
+  deep =  16
+  over = -16
+
+  item_kind = "key"
+
+  x_fit = "frame"
+  y_fit = "top"
+}
