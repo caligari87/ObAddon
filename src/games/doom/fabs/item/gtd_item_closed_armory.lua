637d871d06f2bdf2e936af9f6ce5ef8a61f4aeac
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-16 15:14:32 +0800

    - Created new "sealed armory" item closet. - Fixed misaligning use of COMPTALL in the original toilet fabs.

---
+++ src/games/doom/fabs/item/gtd_item_closed_armory.lua
@@ -0,0 +1,26 @@
+--
+-- Yes, I am aware I am a bad person.
+--
+
+PREFABS.Item_sealed_armory_room =
+{
+  file   = "item/gtd_item_closed_armory.wad"
+  map    = "MAP01"
+
+  rank   = 2
+  prob   = 150
+  theme  = "!hell"
+  env    = "!cave"
+
+  where  = "seeds"
+  seed_w = 3
+  seed_h = 2
+
+  deep =  16
+  over = -16
+
+  item_kind = "key"
+
+  x_fit = "frame"
+  y_fit = "top"
+}
