e7927febd8e1d74e8465388f0611d676d2149978
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-18 16:33:48 +0800

    - Added an amazing new item-container toilet fab!!! Can't EVER get enough of more toilets, you know!

---
+++ src/games/doom/fabs/item/gtd_item_closet_toilet_point.lua
@@ -0,0 +1,23 @@
+PREFABS.Item_closet_toilet_point =
+{
+  file  = "item/gtd_item_closet_toilet_point.wad"
+
+  prob  = 25
+  theme = "!hell"
+
+  where = "point"
+  height = 128
+  size  = 96
+
+  z_fit = "top"
+}
+
+PREFABS.Item_closet_toilet_point_key_carrier =
+{
+  template = "Item_closet_toilet_point"
+
+  rank = 2
+  prob = 75
+
+  item_kind = "key"
+}
