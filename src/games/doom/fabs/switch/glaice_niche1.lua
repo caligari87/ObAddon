40cd842a56026479416505d0fcc8b2894af53019
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-18 11:41:35 +0800

    - Added lua definitions to Glaice's new niche1 switch fab

---
+++ src/games/doom/fabs/switch/glaice_niche1.lua
@@ -0,0 +1,89 @@
+PREFABS.Switch_glaice_niche1 =
+{
+  file   = "switch/glaice_niche1.wad"
+  prob   = 50
+
+  theme  = "tech"
+
+  where  = "seeds"
+
+  seed_w = 1
+  seed_h = 2
+
+  deep = 16
+
+  tag_1 = "?switch_tag"
+
+  x_fit = "frame"
+  y_fit = "top"
+
+  tex_SW1GRAG =
+  {
+    SW1BLUE = 50
+    SW1BRIK = 50
+    SW1BRN2 = 50
+    SW1BRNGN = 50
+    SW1CMT = 50
+    SW1GRAY = 50
+    SW1GRAY1 = 50
+    SW1METAL = 50
+    SW1SLAD = 50
+    SW1STRTN = 50
+    SW1TEK = 50
+  }
+}
+
+PREFABS.Switch_glaice_niche1_urban =
+{
+  template = "Switch_glaice_niche1"
+
+  theme = "urban"
+
+  tex_SW1GRAG =
+  {
+    SW1BLUE = 50
+    SW1BRIK = 50
+    SW1BRN2 = 50
+    SW1BRNGN = 50
+    SW1CMT = 50
+    SW1GRAY = 50
+    SW1GRAY1 = 50
+    SW1METAL = 50
+    SW1SLAD = 50
+    SW1STRTN = 50
+    SW1TEK = 50
+    SW1GRAG = 50
+    SW1GSTON = 50
+    SW1HOT = 50
+    SW1LION = 50
+    SW1MARB = 50
+    SW1PANEL = 50
+    SW1ROCK = 50
+    SW1SATYR = 50
+    SW1WDMET = 50
+    SW1WOOD = 50
+    SW1ZIM = 50
+  }
+}
+
+PREFABs.Switch_glaice_niche1_hell =
+{
+  template = "Switch_glaice_niche1"
+
+  theme = "hell"
+
+  tex_SW1GRAG =
+  {
+    SW1GRAG = 50
+    SW1GSTON = 50
+    SW1HOT = 50
+    SW1LION = 50
+    SW1MARB = 50
+    SW1PANEL = 50
+    SW1ROCK = 50
+    SW1SATYR = 50
+    SW1WDMET = 50
+    SW1WOOD = 50
+    SW1ZIM = 50
+  }
+}
