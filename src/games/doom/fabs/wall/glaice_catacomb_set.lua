e84d3f31eabcf6754538f457df512c7e70dccbb9
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-04-23 01:09:49 -0400

    Updated catacomb prefab
    Added support for Doom1 in the LUA and deleted MAP02, renamed MAP03 into MAP02.

--- src/games/doom/fabs/wall/glaice_catacomb_set.lua
+++ src/games/doom/fabs/wall/glaice_catacomb_set.lua
@@ -2,6 +2,7 @@ PREFABS.Wall_glaice_catacomb_straight =
 {
   file   = "wall/glaice_catacomb_set.wad"
   map    = "MAP01"
+  theme  = "hell"
 
   prob   = 50
   env = "building"
@@ -18,13 +19,40 @@ PREFABS.Wall_glaice_catacomb_straight =
   bound_z2 = 128
 
   z_fit  = "frame"
+
+  tex_CATACMB1 = { CATACMB1=50, CATACMB4=50 }
 }
 
 PREFABS.Wall_glaice_catacomb_diagonal =
 {
   template = "Wall_glaice_catacomb_straight"
+  theme = "hell"
 
-  map      = "MAP03"
+  map      = "MAP02"
 
   where    = "diagonal"
+
+  tex_CATACMB1 = { CATACMB1=50, CATACMB4=50 }
+}
+
+-- Versions for Doom1
+PREFABS.Wall_glaice_catacomb_straight_doom1 =
+{
+  template = "Wall_glaice_catacomb_straight"
+  theme = "flesh"
+  game = "doom"
+  map = "MAP01"
+  tex_CATACMB1 = { CATACMB1=50, CATACMB4=50 }
+}
+
+PREFABS.Wall_glaice_catacomb_diagonal_doom1 =
+{
+  template = "Wall_glaice_catacomb_straight"
+  theme = "flesh"
+  game = "doom"
+  map      = "MAP02"
+
+  where    = "diagonal"
+
+  tex_CATACMB1 = { CATACMB1=50, CATACMB4=50 }
 }
