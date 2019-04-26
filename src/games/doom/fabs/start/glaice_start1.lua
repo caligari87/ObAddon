8607f791596b92f8c991e909768b74b475da21a3
Glaice <GlaiceOldSchoolRTS@users.noreply.github.com>
2019-04-25 21:32:19 -0400

    Added hell entries for Glaice's start fab

--- src/games/doom/fabs/start/glaice_start1.lua
+++ src/games/doom/fabs/start/glaice_start1.lua
@@ -17,6 +17,21 @@ PREFABS.Start_glaice_start_center_pillar =
   y_fit = "top"
 }
 
+PREFABS.Start_glaice_start_center_pillar_hell =
+{
+  template  = "Start_glaice_start_center_pillar"
+  prob = 500
+  map = "MAP01"
+  theme = "hell"
+
+  tex_TEKWALL1 = { SKIN2=50, SKINFACE=50, MARBLE1=50, GSTVINE1=50, GSTVINE2=50, GSTONE1=50, SP_HOT1=50 }
+  tex_SHAWN2 = "METAL"
+  tex_COMPSPAN = { STONE2=50, STONE3=50 }
+  flat_FLAT4 = "FLAT1"
+  flat_CEIL5_1 = "FLOOR6_2"
+
+}
+
 PREFABS.Start_glaice_start_lift =
 {
   file  = "start/glaice_start1.wad"
@@ -35,3 +50,21 @@ PREFABS.Start_glaice_start_lift =
   x_fit = "frame"
   y_fit = "top"
 }
+
+PREFABS.Start_glaice_start_lift_hell =
+{
+  template  = "Start_glaice_start_lift"
+  prob = 500
+  map = "MAP02"
+  theme = "hell"
+
+  tex_SW1COMM = "SW1DIRT"
+  tex_DOORSTOP = "METAL"
+  tex_SHAWN2 = "METAL"
+  tex_SUPPORT2 = "SUPPORT3"
+  tex_TEKWALL4 = { MARBLE2=50, MARBLE3=50 }
+  tex_COMPSPAN = { STONE2=50, STONE3=50 }
+  flat_FLAT4 = "FLAT1"
+  flat_CEIL5_1 = "FLOOR7_2"
+
+  }
