2a7aab72fb8b8f0984147cf959d136e692209cfd
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-28 12:10:07 +0800

    - Added a new toilet fab for hell. REMEMBER, WE CAN NEVER HAVE ENOUGH TOILET FABS.

---
+++ src/games/doom/fabs/item/gtd_item_closet_toilet_hell.lua
@@ -0,0 +1,27 @@
+PREFABS.Item_closet_hellish_loo =
+{
+  file   = "item/gtd_item_closet_toilet_hell.wad"
+  map    = "MAP01"
+
+  rank   = 2
+  prob   = 50
+  theme  = "hell"
+  env    = "outdoor"
+  open_to_sky = true
+
+  where  = "seeds"
+  seed_w = 2
+  seed_h = 2
+
+  item_kind = "key"
+
+  x_fit = "frame"
+  z_fit = { 104,112 }
+}
+
+PREFABS.Item_closet_hellish_loo_shittier =
+{
+  template = "Item_closet_hellish_loo"
+
+  map = "MAP02"
+}
