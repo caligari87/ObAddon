e3c11c9d82318d480c35d5f4aba97ab40ef59eb4
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-28 13:34:19 +0800

    - Added Craneo's pole stage fab as an point item fab!

---
+++ src/games/doom/fabs/item/craneo_item_pole_stage.lua
@@ -0,0 +1,13 @@
+PREFABS.Item_striptease_pole_stage =
+{
+  file  = "item/craneo_item_pole_stage.wad"
+
+  map   = "MAP01"
+
+  prob  = 20
+  theme = "urban"
+
+  where = "point"
+  height = 128
+  size  = 224
+}
