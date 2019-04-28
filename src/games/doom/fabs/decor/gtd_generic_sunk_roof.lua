6f32c014e893441abb5fd99ab12aa0162bae57eb
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-28 22:43:20 +0800

    - Added new octagonal gazebo decor fab.

--- src/games/doom/fabs/decor/gtd_generic_sunk_roof.lua
+++ src/games/doom/fabs/decor/gtd_generic_sunk_roof.lua
@@ -3,8 +3,7 @@ PREFABS.Decor_generic_sunk_roof =
   file   = "decor/gtd_generic_sunk_roof.wad"
   map    = "MAP01"
 
-  prob   = 5000
-  env    = "!outdoor"
+  prob   = 3500
 
   where  = "point"
   size   = 64
@@ -40,58 +39,18 @@ PREFABS.Decor_generic_sunk_roof =
 
 PREFABS.Decor_generic_sunk_roof_yuge =
 {
-  file   = "decor/gtd_generic_sunk_roof.wad"
+  template = "Decor_generic_sunk_roof"
+
   map    = "MAP02"
 
-  prob   = 5000
-  env    = "!outdoor"
-
-  where  = "point"
   size   = 128
-  height = 128
-
-  bound_z1 = 0
-  bound_z2 = 128
-
-  z_fit = "top"
-
-  flat_FLAT2 =
-  {
-    FLAT2=50
-    CEIL1_2=50
-    CEIL1_3=50
-    CEIL3_4=50
-    CEIL3_6=50
-    FLAT17=50
-    FLAT22=50
-    GRNLITE1=50
-    TLITE6_1=50
-    TLITE6_4=50
-    TLITE6_5=50
-    TLITE6_6=50
-  }
-
-  tex_GRAY7 = "METAL"
-
-  sector_1  = { [0]=90, [1]=15 }
-
-  sink_mode = "never"
 }
 
-PREFABS.Decor_generic_sunk_roof_outdoor =
+PREFABS.Decor_generic_sunk_roof_octagon =
 {
   template = "Decor_generic_sunk_roof"
 
-  env = "outdoor"
+  map = "MAP03"
 
-  flat__CEIL = "CEIL5_1"
-}
-
-PREFABS.Decor_generic_sunk_roof_yuge_outdoor =
-{
-  template = "Decor_generic_sunk_roof_yuge"
-
-  env = "outdoor"
-
-  flat__CEIL = "CEIL5_1"
+  size   = 128
 }
