54fce6345cb1d8104ecacb7b979ea5c7897f35dd
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-30 12:14:27 +0800

    - Added simple "sandbag" freestanding cage fabs.

---
+++ src/games/doom/fabs/cage/gtd_cage_sandbag.lua
@@ -0,0 +1,21 @@
+PREFABS.Cage_sandbag_fort =
+{
+  file   = "cage/gtd_cage_sandbag.wad"
+  map    = "MAP01"
+
+  prob   = 50
+
+  where  = "point"
+  size   = 128
+
+  bound_z1 = 0
+}
+
+PREFABS.Cage_sandbag_fort_for_two =
+{
+  template = "Cage_sandbag_fort"
+
+  map      = "MAP02"
+
+  size     = 96
+}
