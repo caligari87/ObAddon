bb9dc27ca2a95690d692d84fcb917076841c7c5b
GTD-Carthage <gtdcarthage@yahoo.com>
2019-04-30 12:30:43 +0800

    - Added new cage seed fab: bunkers!

---
+++ src/games/doom/fabs/cage/gtd_cage_bunker.lua
@@ -0,0 +1,24 @@
+--
+-- Wall Cage
+--
+
+PREFABS.Cage_Wall =
+{
+  file   = "cage/gtd_cage_bunker.wad"
+
+  prob  = 800
+
+  where  = "seeds"
+  shape  = "U"
+
+  seed_w = 1
+  seed_h = 1
+
+  deep   =  16
+  over   = -16
+
+  x_fit = { 48,80 }
+  y_fit = "top"
+
+  sector_8  = { [8]=60, [2]=10, [3]=10, [17]=10, [21]=5 }
+}
