4213a49e56fbc3bd0be44f5e415b78d353ceed13
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-02 12:38:07 +0800

    - Added new hologram table decor thing for tech and urban.

---
+++ src/games/doom/fabs/decor/gtd_decor_tech_hologram_table.lua
@@ -0,0 +1,19 @@
+PREFABS.Decor_tech_hologram_table =
+{
+  file   = "decor/gtd_decor_tech_hologram_table.wad"
+  map    = "MAP01"
+
+  where  = "point"
+
+  prob   = 5000
+  theme  = "!hell"
+  env    = "building"
+
+  size   = 144
+  height = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit = "top"
+}
