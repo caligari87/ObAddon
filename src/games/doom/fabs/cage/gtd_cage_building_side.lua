8480bc13e82ba5a2433b430578fbeaf0f98eee6f
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-05 17:13:19 +0800

    - Added building terrace cage type, inspired by the stuff in some Valiant maps.

---
+++ src/games/doom/fabs/cage/gtd_cage_building_side.lua
@@ -0,0 +1,44 @@
+PREFABS.Cage_wall_building_terrace =
+{
+  file   = "cage/gtd_cage_building_side.wad"
+
+  prob  = 1500
+
+  theme  = "urban"
+  env    = "outdoor"
+
+  where  = "seeds"
+  shape  = "U"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep   =  16
+  over   = -16
+
+  x_fit = "stretch"
+  y_fit = "top"
+
+  tex_CITY01 =
+  {
+    CITY01 = 50
+    CITY02 = 50
+    CITY03 = 50
+    CITY04 = 50
+    CITY05 = 50
+    CITY06 = 50
+    CITY07 = 50
+    CITY08 = 50
+    CITY09 = 50
+    CITY10 = 50
+  }
+
+  tex_RAIL1 =
+  {
+    RAIL1=50
+    MIDWIND6=10
+    FENCE2=10
+    FENCE3=10
+
+  }
+}
