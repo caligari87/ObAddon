f733076c99b9a582b560c07aa774a3fcf867b7ac
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-07 22:35:28 +0800

    - Added switch-operated bridge joiner!

---
+++ src/games/doom/fabs/joiner/gtd_switched_bridge.lua
@@ -0,0 +1,21 @@
+PREFABS.Joiner_switched_bridge =
+{
+  file   = "joiner/gtd_switched_bridge.wad"
+  map    = "MAP01"
+
+  prob   = 150
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep = 16
+  over = 16
+
+  x_fit = { 56,72 , 184,200}
+  y_fit = { 64,96 }
+
+  can_flip = true
+}
