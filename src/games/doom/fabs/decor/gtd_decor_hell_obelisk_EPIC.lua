605b4012094262073d21100c8f644687de247029
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-08 11:53:07 +0800

    - Added new obelisk decorative point fab for hell (red and yellow variations) using Epic textures.

---
+++ src/games/doom/fabs/decor/gtd_decor_hell_obelisk_EPIC.lua
@@ -0,0 +1,23 @@
+PREFABS.Decor_hell_obelisk_EPIC_red =
+{
+  file   = "decor/gtd_decor_hell_obelisk_EPIC.wad"
+  map    = "MAP01"
+
+  prob   = 5000
+  theme  = "hell"
+  env    = "outdoors"
+
+  uses_epic_textures = true
+
+  where  = "point"
+  size   = 96
+  height = 352
+
+  bound_z1 = 0
+}
+
+PREFABS.Decor_hell_obelisk_EPIC_yellow =
+{
+  template = "Decor_hell_obelisk_EPIC_red"
+  map      = "MAP02"
+}
