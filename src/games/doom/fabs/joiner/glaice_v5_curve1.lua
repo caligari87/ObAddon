5e52e345256d1a25580c3cf6774fb50e2e13f757
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-09 10:47:55 +0800

    - Added Lua definitions for the new v5 curve joiner fab.

---
+++ src/games/doom/fabs/joiner/glaice_v5_curve1.lua
@@ -0,0 +1,20 @@
+PREFABS.Joiner_glaice_v5_curve1 =
+{
+  file   = "joiner/glaice_v5_curve1.wad"
+  map    = "MAP01"
+
+  prob   = 30
+  style  = "steepness"
+
+  env      = "!cave"
+  neighbor = "!cave"
+
+  where  = "seeds"
+  shape  = "L"
+
+  seed_w = 2
+  seed_h = 2
+
+  delta_h  = 80
+  nearby_h = 128
+}
