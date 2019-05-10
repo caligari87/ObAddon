35946ca074b75052991686315c8d290463535cf6
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-10 23:52:24 +0800

    - Added new generic outdoor billboard decor fab.

---
+++ src/games/doom/fabs/decor/gtd_decor_outdoor_billboards.lua
@@ -0,0 +1,19 @@
+PREFABS.Decor_outdoor_billboard_UAC =
+{
+  file   = "decor/gtd_decor_outdoor_billboards.wad"
+  map    = "MAP01"
+
+  where  = "point"
+
+  prob   = 3500
+  theme  = "!hell"
+  env    = "outdoor"
+
+  size   = 104
+  height = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit = { 24,48 }
+}
