9a42aa084273c64e635628d5b99b6ea1917481f0
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-10 11:22:53 +0800

    - Added some new tech-themed ceiling light fabs!

---
+++ src/games/doom/fabs/decor/gtd_ceil_light_tech.lua
@@ -0,0 +1,71 @@
+PREFABS.Light_gtd_detailed =
+{
+  file   = "decor/gtd_ceil_light_tech.wad"
+  map    = "MAP01"
+
+  prob   = 200
+  theme  = "!hell"
+  env    = "building"
+
+  kind   = "light"
+  where  = "point"
+
+  height = 128
+
+  bound_z1 = -32
+  bound_z2 = 0
+
+  -- use the occasional-blink FX (fairly rarely)
+  sector_1  = { [0]=90, [1]=15 }
+
+}
+
+PREFABS.Light_gtd_detailed_alt =
+{
+  template = "Light_gtd_detailed"
+
+  tex_TLITE6_6 = "TLITE6_5"
+}
+
+PREFABS.Light_gtd_flourescent_lamp =
+{
+  template = "Light_gtd_detailed"
+
+  map = "MAP02"
+
+  bound_z1 = -24
+}
+
+PREFABS.Light_gtd_flourescent_lamp_alt =
+{
+  template = "Light_gtd_detailed"
+
+  map = "MAP02"
+
+  bound_z1 = -24
+
+  tex_LITE3 = "LITEBLU4"
+  tex_LITE5 = "LITEBLU4"
+  flat_FLAT23 = "FLAT14"
+}
+
+PREFABS.Light_gtd_round =
+{
+  template = "Light_gtd_detailed"
+
+  map = "MAP03"
+
+  bound_z1 = -16
+}
+
+PREFABS.Light_gtd_round_alt =
+{
+  template = "Light_gtd_detailed"
+
+  map = "MAP03"
+
+  bound_z1 = -16
+
+  tex_LITE5 = "LITEBLU4"
+  flat_FLAT23 = "FLAT22"
+}
