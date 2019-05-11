5b0a989a1a3d573529dd42da7d116e04e3a19aea
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-11 18:06:20 +0800

    - Added new gothic chandelier thing for non-tech themes (may need more physical variations later on, currently out of ideas)

---
+++ src/games/doom/fabs/decor/gtd_ceil_light_gothic.lua
@@ -0,0 +1,31 @@
+PREFABS.Light_gtd_gothic_chandelier =
+{
+  file   = "decor/gtd_ceil_light_gothic.wad"
+  map    = "MAP01"
+
+  prob   = 50
+  theme  = "!tech"
+  env    = "building"
+
+  kind   = "light"
+  where  = "point"
+
+  height = 128
+
+  bound_z1 = -48
+  bound_z2 = 0
+}
+
+PREFABS.Light_gtd_gothic_chandelier_alt =
+{
+  template = "Light_gtd_gothic_chandelier"
+
+  tex_FIRELAVA = "COMPBLUE"
+}
+
+PREFABS.Light_gtd_gothic_chandelier_alt_2 =
+{
+  template = "Light_gtd_gothic_chandelier"
+
+  tex_FIRELAVA = "SFALL1"
+}
