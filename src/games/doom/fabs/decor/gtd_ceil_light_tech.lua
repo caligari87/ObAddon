e241327e366662e823e2727bb9d3cd2ce636c4fc
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-11 13:05:52 +0800

    - Added another 2nd alternate version to the original tech light fab.

--- src/games/doom/fabs/decor/gtd_ceil_light_tech.lua
+++ src/games/doom/fabs/decor/gtd_ceil_light_tech.lua
@@ -3,7 +3,7 @@ PREFABS.Light_gtd_detailed =
   file   = "decor/gtd_ceil_light_tech.wad"
   map    = "MAP01"
 
-  prob   = 80
+  prob   = 60
   theme  = "!hell"
   env    = "building"
 
@@ -20,6 +20,7 @@ PREFABS.Light_gtd_detailed =
 
 }
 
+-- red circles with green cover
 PREFABS.Light_gtd_detailed_alt =
 {
   template = "Light_gtd_detailed"
@@ -29,10 +30,21 @@ PREFABS.Light_gtd_detailed_alt =
   tex_BRONZE1 = "BROWNGRN"
 }
 
+-- gray light, gray cover version
+PREFABS.Light_gtd_detailed_alt2 =
+{
+  template = "Light_gtd_detailed"
+
+  flat_TLITE6_6 = "FLAT2"
+  flat_CEIL5_2 = "FLAT19"
+  tex_BRONZE1 = "GRAY7"
+}
+
 PREFABS.Light_gtd_flourescent_lamp =
 {
   template = "Light_gtd_detailed"
   map = "MAP02"
+  prob = 80
 
   bound_z1 = -24
 }
@@ -41,6 +53,7 @@ PREFABS.Light_gtd_flourescent_lamp_alt =
 {
   template = "Light_gtd_detailed"
   map = "MAP02"
+  prob = 80
 
   bound_z1 = -24
 
@@ -53,6 +66,7 @@ PREFABS.Light_gtd_round =
 {
   template = "Light_gtd_detailed"
   map = "MAP03"
+  prob = 80
 
   bound_z1 = -16
 }
@@ -61,6 +75,7 @@ PREFABS.Light_gtd_round_alt =
 {
   template = "Light_gtd_detailed"
   map = "MAP03"
+  prob = 80
 
   bound_z1 = -16
 
@@ -72,6 +87,7 @@ PREFABS.Light_gtd_tall_light =
 {
   template = "Light_gtd_detailed"
   map = "MAP04"
+  prob = 80
 
   bound_z1 = -88
 
@@ -82,6 +98,7 @@ PREFABS.Light_gtd_tall_light_alt =
 {
   template = "Light_gtd_detailed"
   map = "MAP04"
+  prob = 80
 
   bound_z1 = -88
 
