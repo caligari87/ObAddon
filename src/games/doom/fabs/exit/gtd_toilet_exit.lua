bf7d8df474548be0f6d86968ae3a3d89fb0f7070
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-12 12:00:35 +0800

    - Added new toilet-themed start and exit fabs.

---
+++ src/games/doom/fabs/exit/gtd_toilet_exit.lua
@@ -0,0 +1,20 @@
+PREFABS.Exit_gtd_toilet =
+{
+  file   = "exit/gtd_toilet_exit.wad"
+  map    = "MAP01"
+
+  prob   = 100000
+
+  theme  = "!hell"
+
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 3
+
+  deep   =  16
+  over   = -16
+
+  x_fit  = "frame"
+  y_fit  = "top"
+}
