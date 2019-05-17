9c4a4a30508f4fd4e91be71fa57062135c191097
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-17 14:48:38 +0800

    - Lua normalization

--- scripts/normalize-source.sh
+++ scripts/normalize-source.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-find .. -iname \*.lua -exec sed -i -E -e 's/\r//' -e 's/\s+$//' -e 's/	/    /g' -e '$a\' \{\} \;
+find .. -iname \*.lua -exec sed -i -E -e 's/\r//' -e 's/\s+$//' -e 's/	/    /g' \{\} \;
+find .. -iname \*.lua | while read f; do
+	tail -c1 "$f" | read -r _ || echo >> "$f"
+done
