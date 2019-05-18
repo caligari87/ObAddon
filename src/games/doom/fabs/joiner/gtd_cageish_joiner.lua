695e7d1e46c38a1fe1b33f02fcae57f59fd14aa2
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-18 14:26:55 +0800

    - Added new switched cage-ish joiner.

---
+++ src/games/doom/fabs/joiner/gtd_cageish_joiner.lua
@@ -0,0 +1,19 @@
+PREFABS.Joiner_gtd_cageish_joiner_bunker =
+{
+  file   = "joiner/gtd_cageish_joiner.wad"
+  map    = "MAP01"
+
+  prob   = 150
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+  over = 16
+
+  x_fit = { 80,96 , 160,176 }
+  y_fit = "frame"
+}
