6f3492181069c4cebb4b168c89dced638483b5ae
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-20 22:37:49 +0800

    - Increased probability chance for the cage-ish joiners.

--- src/games/doom/fabs/joiner/gtd_cageish_joiner.lua
+++ src/games/doom/fabs/joiner/gtd_cageish_joiner.lua
@@ -3,7 +3,7 @@ PREFABS.Joiner_gtd_cageish_joiner_bunker =
   file   = "joiner/gtd_cageish_joiner.wad"
   map    = "MAP01"
 
-  prob   = 125
+  prob   = 175
 
   where  = "seeds"
   shape  = "I"
@@ -34,7 +34,7 @@ PREFABS.Joiner_gtd_cageish_joiner_2_standing =
   template = "Joiner_gtd_cageish_joiner_bunker"
   map = "MAP03"
 
-  prob = 75
+  prob = 110
 
   x_fit = "frame"
 }
@@ -44,7 +44,7 @@ PREFABS.Joiner_gtd_cageish_joiner_4_standing =
   template = "Joiner_gtd_cageish_joiner_bunker"
   map = "MAP04"
 
-  prob = 75
+  prob = 110
 
   seed_w = 3
 
