b8157b2dc6bf5626d609cd037eb821e98aa3a37d
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-23 08:26:14 +0800

    - Added Beed28's broken door fab as a 'picture' closet decor fab.

---
+++ src/games/doom/fabs/picture/beed28_broken_door.lua
@@ -0,0 +1,31 @@
+PREFABS.Pic_beed28_broken_door =
+{
+  file   = "picture/beed28_broken_door.wad"
+  map    = "MAP01"
+
+  prob   = 20
+  env   = "building"
+  theme = "!hell"
+
+  where  = "seeds"
+  height = 128
+
+  seed_w = 2
+  seed_h = 1
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  deep   =  16
+  over   = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+
+  tex_BIGDOOR2 =
+  {
+    BIGDOOR2 = 50
+    BIGDOOR3 = 50
+    BIGDOOR4 = 50
+  }
+}
