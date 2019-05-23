114377711097255fd0e066a7bdda3c3ca199756b
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-23 08:15:30 +0800

    - Added new confession booth themed item container fab for hell and urban. CONFESS!!! CONFESSSSSS!!!!!

---
+++ src/games/doom/fabs/item/gtd_item_confession_booth.lua
@@ -0,0 +1,19 @@
+PREFABS.Item_closet_confession_booth =
+{
+  file   = "item/gtd_item_confession_booth.wad"
+  map    = "MAP01"
+
+  prob   = 25
+  theme  = "!tech"
+  env    = "!cave"
+
+  where  = "seeds"
+  seed_w = 1
+  seed_h = 1
+
+  deep = 16
+  over = 16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
