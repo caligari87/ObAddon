ad392e2b66598546983b5f0190925c5a19853d08
GTD-Carthage <gtdcarthage@yahoo.com>
2019-05-23 11:32:09 +0800

    - Added new wallmachine decor for tech and urban themes.

--- src/games/doom/fabs/picture/gtd_pic_tech_wallmachines.lua
+++ src/games/doom/fabs/picture/gtd_pic_tech_wallmachines.lua
@@ -3,7 +3,7 @@ PREFABS.Pic_techy_vats =
   file   = "picture/gtd_pic_tech_wallmachines.wad"
   map    = "MAP01"
 
-  prob   = 25 --35
+  prob   = 25
   theme = "!hell"
 
   where  = "seeds"
@@ -28,13 +28,12 @@ PREFABS.Pic_techy_vats =
   }
 }
 
-
 PREFABS.Pic_pipagery =
 {
   file   = "picture/gtd_pic_tech_wallmachines.wad"
   map    = "MAP02"
 
-  prob   = 25 --35
+  prob   = 25
   theme = "!hell"
   env = "building"
 
@@ -52,4 +51,36 @@ PREFABS.Pic_pipagery =
 
   x_fit = "frame"
   y_fit = "top"
+}
+
+PREFABS.Pic_computer_pull_rack_thingies =
+{
+  file   = "picture/gtd_pic_tech_wallmachines.wad"
+  map    = "MAP03"
+
+  prob   = 25
+  theme = "tech"
+  env = "building"
+
+  where  = "seeds"
+  height = 128
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+  over = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
+
+PREFABS.Pic_computer_charging_grid_thingies =
+{
+  template = "Pic_computer_pull_rack_thingies"
+  map      = "MAP04"
+
+  theme  = "!hell"
+
+  seed_w = 3
 }
