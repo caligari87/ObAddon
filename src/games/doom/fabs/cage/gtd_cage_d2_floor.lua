fbb54a6d5465406e2cf067aad89c2a9908781c14
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-10 09:55:18 +0800

    - Added new cage type inspired by some elements in Doom 2.

---
+++ src/games/doom/fabs/cage/gtd_cage_d2_floor.lua
@@ -0,0 +1,29 @@
+PREFABS.Cage_gtd_D2_floor =
+{
+  file   = "cage/gtd_cage_d2_floor.wad"
+
+  prob  = 100
+  theme = "hell"
+
+  where  = "seeds"
+  shape  = "U"
+
+  seed_w = 1
+  seed_h = 1
+
+  deep   =  16
+  over   = -16
+
+  x_fit = "stretch"
+  y_fit = "top"
+}
+
+PREFABS.Cage_gtd_D2_floor_tech =
+{
+  template = "Cage_gtd_D2_floor"
+
+  theme = "!hell"
+
+  tex_SUPPORT3 = "SHAWN2"
+  tex_METAL = "SHAWN2"
+}
