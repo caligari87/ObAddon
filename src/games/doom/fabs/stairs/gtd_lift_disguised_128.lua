641b7620136b54d85ce782bca34455f2184a0959
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-10 21:43:22 +0800

    - Added llift disguised to be part of the platform. (inspired by Memento Mori?)

---
+++ src/games/doom/fabs/stairs/gtd_lift_disguised_128.lua
@@ -0,0 +1,40 @@
+-- MSSP: I would like to pretend this was inspired
+-- Memento Mori somehow.
+
+PREFABS.Lift_gtd_disguised_128 =
+{
+  file = "stairs/gtd_lift_disguised_128.wad"
+  map = "MAP01"
+
+  prob = 15
+  style = "steepness"
+
+  where = "seeds"
+  shape = "I"
+
+  seed_h = 1
+  seed_w = 1
+
+  x_fit = "frame"
+
+  bound_z1 = 0
+
+  delta_h = 128
+  plain_ceiling = true
+}
+
+PREFABS.Lift_gtd_disguised_128_2h =
+{
+  template = "Lift_gtd_disguised_128"
+  map = "MAP02"
+
+  seed_h = 2
+}
+
+PREFABS.Lift_gtd_disguised_128_3h =
+{
+  template = "Lift_gtd_disguised_128"
+  map = "MAP03"
+
+  seed_h = 3
+}
