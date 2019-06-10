d495e8433585231099a28df2f54b61e72c5fb9ee
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-11 00:01:08 +0800

    - Added new item balcony (key-containing) with a cage around it.

---
+++ src/games/doom/fabs/item/gtd_item_cage_balcony.lua
@@ -0,0 +1,19 @@
+PREFABS.Item_cage_balcony =
+{
+  file = "item/gtd_item_cage_balcony.wad"
+
+  rank = 2
+  prob = 250
+
+  item_kind = "key"
+
+  where  = "seeds"
+  seed_w = 2
+  seed_h = 2
+
+  deep = 16
+  over = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
