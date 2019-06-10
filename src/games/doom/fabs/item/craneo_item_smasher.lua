0b1beec8f5eeb8677240030bef00523b1b251286
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-10 09:32:17 +0800

    - Fixed issue with Mogawaltz's secret eye fabs being on the incorrect origin. - Added Craneo's item smasher fab.

---
+++ src/games/doom/fabs/item/craneo_item_smasher.lua
@@ -0,0 +1,16 @@
+PREFABS.Item_craneo_smasher =
+{
+  file  = "item/craneo_item_smasher.wad"
+  where = "point"
+
+  map = "MAP01"
+
+  prob = 8
+}
+
+PREFABS.Item_craneo_smasher_alt =
+{
+  template = "Item_craneo_smasher"
+
+  map = "MAP02"
+}
