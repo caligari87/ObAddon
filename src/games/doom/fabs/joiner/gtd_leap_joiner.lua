dafdb173ef2cba55ed803aaf8d7f8dbda2e5115f
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-11 14:06:17 +0800

    - Added new leap-themed joiner. (really just walking running across the ledge)

---
+++ src/games/doom/fabs/joiner/gtd_leap_joiner.lua
@@ -0,0 +1,21 @@
+PREFABS.Joiner_leap =
+{
+  file = "joiner/gtd_leap_joiner.wad"
+  map = "MAP01"
+
+  prob = 300
+
+  where = "seeds"
+  shape = "I"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+  over = 16
+
+  bound_z1 = -64
+
+  x_fit = { 112,144 }
+  y_fit = { 16,24 , 136,144 }
+}
