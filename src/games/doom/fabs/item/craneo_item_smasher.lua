b32c4c487b17369b767343cbcacdc697c7d5b4fe
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-11 22:20:24 +0800

    - Fixed various issues with Craneo's item smasher fab, including spawning in tight spots (because no size property) and unusual texture alignments.

--- src/games/doom/fabs/item/craneo_item_smasher.lua
+++ src/games/doom/fabs/item/craneo_item_smasher.lua
@@ -1,16 +1,45 @@
-PREFABS.Item_craneo_smasher =
+PREFABS.Item_craneo_smasher_tech =
 {
   file  = "item/craneo_item_smasher.wad"
   where = "point"
 
+  theme = "!hell"
+
   map = "MAP01"
+  env = "building"
+
+  size = 72
 
   prob = 8
 }
 
-PREFABS.Item_craneo_smasher_alt =
+PREFABS.Item_craneo_smasher_alt_tech =
 {
-  template = "Item_craneo_smasher"
+  template = "Item_craneo_smasher_tech"
 
   map = "MAP02"
+}
+
+PREFABS.Item_craneo_smasher_gothic =
+{
+  template = "Item_craneo_smasher_tech"
+
+  theme = "hell"
+
+  flat_FLAT23 = "CEIL5_2"
+  tex_SHAWN2 = "BIGDOOR5"
+  tex_SUPPORT2 = "GSTONE2"
+}
+
+PREFABS.Item_craneo_smasher_alt_gothic =
+{
+  template = "Item_craneo_smasher_tech"
+
+  map = "MAP02"
+
+  theme = "hell"
+
+  flat_FLAT23 = "CEIL5_2"
+  tex_SHAWN2 = "BIGDOOR5"
+  tex_SUPPORT2 = "GSTONE2"
 }
