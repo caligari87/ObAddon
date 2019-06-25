db83b6058bffba064e0fa9e45a29c78e4cae6441
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-25 15:56:36 +0800

    - Fixed some misaligned textures on the old car fabs. - Added some more road clutter fabs.

---
+++ src/games/doom/fabs/decor/gtd_decor_road_clutter.lua
@@ -0,0 +1,25 @@
+PREFABS.Decor_road_clutter_three_cones =
+{
+  file = "decor/gtd_decor_road_clutter.wad"
+  map = "MAP01"
+
+  prob = 3500
+  theme = "!hell"
+
+  can_be_on_roads = "only"
+
+  where = "point"
+  size = 64
+  height = 94
+
+  bound_z1 = 0
+  bound_z2 = 94
+}
+
+PREFABS.Decor_road_clutter_one_cone =
+{
+  template = "Decor_road_clutter_three_cones"
+  map = "MAP01"
+
+  size = 32
+}
