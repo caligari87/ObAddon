ef87402ba037233172142f401877f35d13bda4c4
GTD-Carthage <gtdcarthage@yahoo.com>
2019-06-26 15:45:17 +0800

    - Modified the old street barrier (was missing the rail texture) - Added new cement barriers for tech and urban (and especially the roads)

--- src/games/doom/fabs/decor/gtd_decor_road_clutter.lua
+++ src/games/doom/fabs/decor/gtd_decor_road_clutter.lua
@@ -5,6 +5,7 @@ PREFABS.Decor_road_clutter_three_cones =
 
   prob = 3500
   theme = "!hell"
+  env = "outdoor"
 
   can_be_on_roads = true
 
@@ -59,4 +60,39 @@ PREFABS.Decor_road_clutter_boxes_alt =
 
   tex_CRATE2 = "WOODMET1"
   tex_CRATINY = "WOOD10"
+}
+
+PREFABS.Decor_road_clutter_concrete_barrier =
+{
+  file = "decor/gtd_decor_road_clutter.wad"
+  map = "MAP04"
+
+  prob = 4000
+  theme = "!hell"
+  env = "outdoor"
+
+  can_be_on_roads = true
+
+  where = "point"
+  size = 80
+  height = 32
+
+  bound_z1 = 0
+  bound_z2 = 32
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_road_clutter_concrete_barrier_striped =
+{
+  template = "Decor_road_clutter_concrete_barrier"
+  map = "MAP05"
+}
+
+PREFABS.Decor_road_clutter_concrete_barrier_striped_double =
+{
+  template = "Decor_road_clutter_concrete_barrier"
+  map = "MAP06"
+
+  size = 100
 }
