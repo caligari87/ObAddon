75d9fb326c5c35567d531bde2f4b11c47caf3b51
GTD-Carthage <gtdcarthage@yahoo.com>
2019-07-02 13:05:27 +0800

    - Attempted to seperate the Lua definition for the vanilla small door into another file.

---
+++ src/games/doom/fabs/door/door1_small.lua
@@ -0,0 +1,29 @@
+PREFABS.Door_small_vanilla =
+{
+  file = "door/door1.wad"
+
+  map  = "MAP01"
+
+  prob = 2000
+
+  where = "edge"
+  seed_w = 2
+
+  deep = 32
+  over = 32
+
+  x_fit = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  flat_TLITE6_6 = { TLITE6_6=50, TLITE6_5=50 }
+
+  sector_1  = { [0]=75, [1]=15, [2]=5, [3]=5, [8]=10, [12]=5, [13]=5 }
+
+  tex_DOOR3 =
+  {
+    DOOR3 = 50
+    DOOR1 = 50
+  }
+}
