a8b2a482e6f1cf3b3c8ffbf36599e9a7526934dd
GTD-Carthage <gtdcarthage@yahoo.com>
2019-07-08 20:16:58 +0800

    - Slightly reduced the probability of the confession booth fab and removed the over property.

--- src/games/doom/fabs/item/gtd_item_confession_booth.lua
+++ src/games/doom/fabs/item/gtd_item_confession_booth.lua
@@ -3,7 +3,7 @@ PREFABS.Item_closet_confession_booth =
   file   = "item/gtd_item_confession_booth.wad"
   map    = "MAP01"
 
-  prob   = 18
+  prob   = 15
   theme  = "!tech"
   env    = "!cave"
 
@@ -12,7 +12,6 @@ PREFABS.Item_closet_confession_booth =
   seed_h = 1
 
   deep = 16
-  over = 16
 
   x_fit = "frame"
   y_fit = "top"
