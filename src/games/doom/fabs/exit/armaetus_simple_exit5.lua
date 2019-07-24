d1c1916bae08cf225e875d6d2bfe72477f42ef87
GTD-Carthage <gtdcarthage@yahoo.com>
2019-07-24 12:22:46 +0800

    - Added Lua definitions for Armaetus's new exit fabs.

---
+++ src/games/doom/fabs/exit/armaetus_simple_exit5.lua
@@ -0,0 +1,21 @@
+PREFABS.Exit_armaetus_simple_exit5_fans =
+{
+  file   = "exit/armaetus_simple_exit5.wad"
+  map    = "MAP01"
+
+  prob   = 200
+
+  theme = "!hell"
+
+  where  = "seeds"
+
+  uses_epic_textures = true
+
+  seed_w = 2
+  seed_h = 2
+
+  deep   = 16
+
+  x_fit  = "frame"
+  y_fit  = "top"
+}
