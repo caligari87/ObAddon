9de87b114b0505867847668591207975869a3b92
GTD-Carthage <gtdcarthage@yahoo.com>
2019-07-26 12:32:53 +0800

    - Added polish and Lua definitions to Armaetus's new switch fabs.

---
+++ src/games/doom/fabs/switch/armaetus_switch_tech.lua
@@ -0,0 +1,13 @@
+PREFABS.Switch_armaetus_tech =
+{
+  file   = "switch/armaetus_switch_tech.wad"
+
+  theme  = "!hell"
+  key = "sw_metal"
+
+  prob   = 50
+
+  where  = "point"
+
+  tag_1  = "?switch_tag"
+}
