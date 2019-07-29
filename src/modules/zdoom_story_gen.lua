57f73cbe404882fd3bceac911533716bc86654e9
GTD-Carthage <gtdcarthage@yahoo.com>
2019-07-29 10:18:49 +0800

    - _RAND_ENTITY_TECH is now set as the keyword for tech entities.

--- src/modules/zdoom_story_gen.lua
+++ src/modules/zdoom_story_gen.lua
@@ -33,7 +33,7 @@ function ZStoryGen_format_story_chunk(story_strings, info)
     story_strings = string.gsub(story_strings, "_RAND_CONTRIBUTOR", info.contributor_name)
     story_strings = string.gsub(story_strings, "_MCGUFFIN_TECH", info.tech_mcguffin)
     story_strings = string.gsub(story_strings, "_MCGUFFIN_HELL", info.hell_mcguffin)
-    story_strings = string.gsub(story_strings, "_ENTITY_TECH", info.tech_entity)
+    story_strings = string.gsub(story_strings, "_RAND_ENTITY_TECH", info.tech_entity)
 
     story_strings = string.gsub(story_strings, "NOUNMEMBERS", info.demon_name)
   end
