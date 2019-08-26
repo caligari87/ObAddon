e5b8090cba50a7c197d08da058826cd2b9401c67
GTD-Carthage <gtdcarthage@yahoo.com>
2019-08-26 15:07:06 +0800

    - Added new variant of the old tech window item closet fab.

--- src/games/doom/fabs/item/gtd_item_tech_window.lua
+++ src/games/doom/fabs/item/gtd_item_tech_window.lua
@@ -31,5 +31,28 @@ PREFABS.Item_tech_window_doom1 =
   game   = "doom"
 
   tex_BRONZE4 = "BROWN96"
+}
 
+PREFABS.Item_tech_window_2 =
+{
+  file   = "item/gtd_item_tech_window.wad"
+  map    = "MAP02"
+
+  game   = "doom2"
+
+  prob   = 50
+  theme  = "!hell"
+  env    = "building"
+
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep =  16
+
+  bound_z1 = 0
+
+  y_fit = "top"
+  x_fit = "frame"
 }
