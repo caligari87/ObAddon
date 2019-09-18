15b6a6a54c79b7c9cba2b2a2e0dad289dd0ce605
GTD-Carthage <gtdcarthage@yahoo.com>
2019-09-18 13:52:57 +0800

    - Fixed some texture and y_fit issues with the downtown facade joiner fab.

--- src/games/doom/fabs/joiner/gtd_downtown_facade_joiner.lua
+++ src/games/doom/fabs/joiner/gtd_downtown_facade_joiner.lua
@@ -25,8 +25,8 @@ PREFABS.Joiner_downtown_facade =
 
   bound_z1 = 0
 
-  x_fit  = { 124,132 }
-  y_fit = "top"
+  x_fit = { 72,184 }
+  y_fit = { 96,136 }
 
   tex_CITY01 =
   {
@@ -87,5 +87,5 @@ PREFABS.Joiner_downtown_facade_flipped =
   env = "building"
   neighbor = "outdoor"
 
-  y_fit = "bottom"
+  y_fit = { 24,64 }
 }
