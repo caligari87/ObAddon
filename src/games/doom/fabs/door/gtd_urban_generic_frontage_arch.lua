65fcd4f586c4a396f71bad57c31194a89d7e4448
GTD-Carthage <gtdcarthage@yahoo.com>
2019-10-20 13:03:29 +0800

    - Increased probability for the generic frontage arch in Doom 2 urban theme.

--- src/games/doom/fabs/door/gtd_urban_generic_frontage_arch.lua
+++ src/games/doom/fabs/door/gtd_urban_generic_frontage_arch.lua
@@ -3,7 +3,7 @@ PREFABS.Arch_generic_urban_frontage =
   file   = "door/gtd_urban_generic_frontage_arch.wad"
   map    = "MAP01"
 
-  prob   = 300
+  prob   = 400
   rank   = 1
 
   theme  = "urban"
@@ -35,7 +35,7 @@ PREFABS.Arch_generic_urban_frontage_alt =
   template = "Arch_generic_urban_frontage"
   map      = "MAP02"
 
-  prob   = 100
+  prob   = 200
 
   seed_w = 2
 }
