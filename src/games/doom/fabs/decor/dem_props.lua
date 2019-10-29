87733d8aa1c111aa3c071892bef01ea6b1c52159
GTD-Carthage <gtdcarthage@yahoo.com>
2019-10-29 10:50:28 +0800

    - Added Demios's litter props and fixes for one of the cage fabs.

--- src/games/doom/fabs/decor/dem_props.lua
+++ src/games/doom/fabs/decor/dem_props.lua
@@ -51,8 +51,6 @@ PREFABS.Decor_mailbox =
   file   = "decor/dem_props.wad"
   map    = "MAP03"
 
-  engine = "zdoom"
-
   prob   = 3500
   theme  = "urban"
   env    = "outdoor"
@@ -60,6 +58,181 @@ PREFABS.Decor_mailbox =
   where  = "point"
   size   = 48
   height = 56
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- litter1
+PREFABS.Decor_litter1 =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP04"
+
+  prob   = 2500
+  theme  = "!hell"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  size   = 64
+  height = 32
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- litter2
+PREFABS.Decor_litter2 =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP05"
+
+  prob   = 2500
+  theme  = "!hell"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  size   = 64
+  height = 32
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- litter3
+PREFABS.Decor_litter3 =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP06"
+
+  prob   = 2500
+  theme  = "!hell"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  size   = 64
+  height = 32
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- litter4
+PREFABS.Decor_litter4 =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP07"
+
+  prob   = 2500
+  theme  = "!hell"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  size   = 64
+  height = 32
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- litter5
+PREFABS.Decor_litter5 =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP08"
+
+  prob   = 2500
+  theme  = "!hell"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  size   = 64
+  height = 32
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- litter6
+PREFABS.Decor_litter6 =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP09"
+
+  prob   = 2500
+  theme  = "!hell"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  size   = 64
+  height = 32
+
+  bound_z1 = 0
+
+  sink_mode = "never"
+
+  face_open = true
+}
+
+-- Drug deal gone bad
+PREFABS.Decor_deal =
+{
+  file   = "decor/dem_props.wad"
+  map    = "MAP10"
+
+  engine = "zdoom"
+
+  prob   = 3500
+  theme  = "urban"
+  env    = "!cave"
+
+  uses_epic_textures = true
+
+  where  = "point"
+  size   = 256
+  height = 32
 
   bound_z1 = 0
 
