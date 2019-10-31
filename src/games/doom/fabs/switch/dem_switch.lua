1625e3b1b1893afd25098686e51ccfaa9bc1d14a
Scionox <spider100160@yahoo.com>
2019-10-31 23:09:41 +0300

    -Added Demios's new switch

---
+++ src/games/doom/fabs/switch/dem_switch.lua
@@ -0,0 +1,20 @@
+PREFABS.Switch_military_radio =
+{
+  file   = "switch/dem_switch.wad"
+
+  engine = "zdoom"
+
+  env    = "outdoor"
+
+  key = "sw_metal"
+  where  = "point"
+
+  size   = 96
+  height = 64
+  uses_epic_textures = true
+
+
+  prob   = 90
+
+  tag_1 = "?switch_tag"
+}
