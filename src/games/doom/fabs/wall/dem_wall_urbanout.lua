8de789416c651b386096d4f2b5587c49ad66e38a
GTD-Carthage <gtdcarthage@yahoo.com>
2019-10-31 21:38:35 +0800

    - Added Demios's new urban wall props and adjustments to existing fabs.

---
+++ src/games/doom/fabs/wall/dem_wall_urbanout.lua
@@ -0,0 +1,239 @@
+
+
+PREFABS.Wall_trash1 =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP01"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 80
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+}
+
+PREFABS.Wall_trash2 =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP02"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 80
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+}
+
+PREFABS.Wall_trash3 =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP03"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 80
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+}
+
+PREFABS.Wall_dumpsteropen =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP04"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 64
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+}
+
+PREFABS.Wall_dumpsterclosed =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP05"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 128
+  deep   = 64
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+}
+
+PREFABS.Wall_doorstopstep =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP06"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 160
+  deep   = 56
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+  tex_DOOR3 =
+  {
+    DOOR1 = 50
+    DOOR3 = 50
+  }
+
+}
+
+PREFABS.Wall_doorstep =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP07"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 160
+  deep   = 56
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+  tex_DOOR3 =
+  {
+    DOOR1 = 50
+    DOOR3 = 50
+  }
+
+}
+
+PREFABS.Wall_boombox =
+{
+  file   = "wall/dem_wall_urbanout.wad"
+  map    = "MAP08"
+
+  prob   = 20
+  theme = "urban"
+  env    = "outdoor"
+
+  uses_epic_textures = true
+
+  on_liquids = "never"
+
+  on_scenics = "never"
+
+  need_solid_back = true
+
+  where  = "edge"
+  height = 128
+  long   = 160
+  deep   = 48
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  z_fit = "top"
+
+}
