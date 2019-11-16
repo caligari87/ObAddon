a59303e40f158d3d3be362246f0c74e253675311
Scionox <spider100160@yahoo.com>
2019-11-17 00:08:25 +0300

    -Added Demios's new urban joiner and a tiny sewers fix

--- src/games/doom/fabs/joiner/dem_urban_joiners.lua
+++ src/games/doom/fabs/joiner/dem_urban_joiners.lua
@@ -159,3 +159,44 @@ PREFABS.Joiner_bedroom2 =
      STUCCO=50, STUCCO1=50, STUCCO3=50
     }
 }
+
+--an appartment stairwell joiner
+PREFABS.Joiner_stairwell =
+{
+  file = "joiner/dem_urban_joiners.wad"
+  map = "MAP05"
+
+  prob = 3000
+
+  theme = "urban"
+
+  env      = "outdoor"
+  neighbor = "outdoor"
+
+  where = "seeds"
+  shape = "I"
+
+  style = "steepness"
+
+  seed_w = 2
+  seed_h = 3
+
+  deep = 16
+  over = 16
+
+  x_fit = "frame"
+  y_fit  = "frame"
+
+  delta_h = 96
+  nearby_h = 272
+
+  can_flip = true
+
+  tex_BRICK9 = {
+    BRICK1=50, BRICK10=50, BRICK11=50,
+    BRICK2=50, BRICK3=50, BRICK4=50,
+     BRICK6=50, BRICK7=50, BRICK8=50,
+     BIGBRIK1=50, BIGBRIK2=50, STONE2=50,
+     STUCCO=50, STUCCO1=50, STUCCO3=50
+    }
+}
