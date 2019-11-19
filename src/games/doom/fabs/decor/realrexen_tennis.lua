6193e661947ba9352d8c17c6da5b2502e2c9f47d
Scionox <spider100160@yahoo.com>
2019-11-19 19:29:54 +0300

    -Added realrexen's new decor fab

---
+++ src/games/doom/fabs/decor/realrexen_tennis.lua
@@ -0,0 +1,16 @@
+PREFABS.Decor_realrexen_tennis =
+{
+  file  = "decor/realrexen_tennis.wad"
+  map   = "MAP01"
+
+  prob  = 2500
+
+  theme = "urban"
+  env   = "building"
+
+  where = "point"
+  size   = 96
+  height = 128
+
+  bound_z1 = 0
+}
