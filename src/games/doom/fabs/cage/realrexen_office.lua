841ee6aca2525808f8d8ed1b6f202e4729fc2792
Scionox <spider100160@yahoo.com>
2019-11-23 14:09:11 +0300

    -Added realrexen's new cage fab

---
+++ src/games/doom/fabs/cage/realrexen_office.lua
@@ -0,0 +1,45 @@
+PREFABS.Cage_realrexen_office =
+{
+  file   = "cage/realrexen_office.wad"
+  map    = "MAP01"
+
+  prob   = 400
+  theme  = "!hell"
+
+  env    = "building"
+
+  where  = "seeds"
+  shape  = "U"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+  y_fit  = "frame"
+  
+  engine = "gzdoom"
+
+   thing_2007 =
+    {
+ --   pistol = 50
+	shotty = 50
+    super = 50
+	chain = 50
+--	bfg = 50
+	launch = 50
+	plasma = 50
+	saw = 50
+   }
+    
+  thing_10 =
+   {
+    dead_imp = 25
+	dead_player = 25
+	dead_zombie = 25
+	dead_shooter = 25
+  }
+  
+}
