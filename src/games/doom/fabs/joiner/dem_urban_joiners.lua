66131507c8b68ab4508ff40d1cda2fc6240b577a
Frozsoul <41276432+Frozsoul2019@users.noreply.github.com>
2019-11-23 19:36:17 -0500

    Demios updates to prefabs

--- src/games/doom/fabs/joiner/dem_urban_joiners.lua
+++ src/games/doom/fabs/joiner/dem_urban_joiners.lua
@@ -243,7 +243,7 @@ PREFABS.Joiner_bar =
     BRICK2=50, BRICK4=50,
     BRICK6=50, BRICK7=50, BRICK8=50,
     STONE2=50, SHAWN4=50, SHAWN5=50,
-    STUCCO=50,  STUCCO1=50,  STUCCO3=50,     STARTAN3=50, STARGR1=50, GRAY7=50,
+    STUCCO=50,  STUCCO1=50,  STUCCO3=50, STARGR1=50, GRAY7=50,
     PANEL6=50, BRIKS40=50, BRIKS43=50,
     GOTH16=50, GOTH31=50, WD03=50
   }
@@ -285,7 +285,7 @@ PREFABS.Joiner_waiting_room =
     BRICK2=50, BRICK4=50,
     BRICK6=50, BRICK7=50, BRICK8=50,
     STONE2=50, SHAWN4=50, SHAWN5=50,
-    STUCCO=50,  STUCCO1=50,  STUCCO3=50,     STARTAN3=50, STARGR1=50, GRAY7=50,
+    STUCCO=50,  STUCCO1=50,  STUCCO3=50, STARGR1=50, GRAY7=50,
     PANEL6=50, BRIKS40=50, BRIKS43=50,
     GOTH16=50, GOTH31=50, WD03=50
   }
@@ -332,9 +332,97 @@ PREFABS.Joiner_electronic_store =
     BRICK2=50, BRICK4=50,
     BRICK6=50, BRICK7=50, BRICK8=50,
     STONE2=50, SHAWN4=50, SHAWN5=50,
-    STUCCO=50,  STUCCO1=50,  STUCCO3=50,     STARTAN3=50, STARGR1=50, GRAY7=50,
+    STUCCO=50,  STUCCO1=50,  STUCCO3=50, STARGR1=50,
     PANEL6=50, BRIKS40=50, BRIKS43=50,
     GOTH16=50, GOTH31=50, WD03=50
+  }
+
+}
+
+
+--a raided cornerstore
+PREFABS.Joiner_cornerstore =
+{
+  file   = "joiner/dem_urban_joiners.wad"
+  map    = "MAP09"
+
+  engine = "zdoom"
+
+  prob   = 1250
+
+  theme  = "urban"
+
+  env      = "outdoor"
+  neighbor = "building"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 3
+
+  deep = 16
+  over = 16
+
+  x_fit = "frame"
+  y_fit  = "frame"
+
+  uses_epic_textures = true
+
+  can_flip = true
+
+  tex_STARTAN1 = {
+    BRICK1=50, BRICK12=50, BRICK11=50,
+    BRICK2=50, BRICK4=50,
+    BRICK6=50, BRICK7=50, BRICK8=50,
+    STONE2=50, STUCCO=50,  STUCCO1=50,
+    STUCCO3=50, STARGR1=50,
+    PANEL6=50, BRIKS40=50, BRIKS43=50,
+    GOTH31=50, BRICK9=50, 
+    BRICK10=50,TANROCK2=50, TANROCK3=50
+  }
+
+}
+
+--a fairly intact bookstore
+PREFABS.Joiner_bookstore =
+{
+  file   = "joiner/dem_urban_joiners.wad"
+  map    = "MAP10"
+
+  engine = "zdoom"
+
+  prob   = 1250
+
+  theme  = "urban"
+
+  env      = "outdoor"
+  neighbor = "building"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 3
+
+  deep = 16
+  over = 16
+
+  x_fit = "frame"
+  y_fit  = "frame"
+
+  uses_epic_textures = true
+
+  can_flip = true
+
+  tex_STARTAN1 = {
+    BRICK1=50, BRICK12=50,
+    BRICK2=50, BRICK4=50,
+    BRICK6=50, BRICK7=50, BRICK8=50,
+    STUCCO=50, STUCCO1=50,
+    STUCCO3=50, BRIKS43=50,
+    GOTH31=50,GOTH16=50,GOTH02=50,
+    BRICK9=50, TANROCK2=50, TANROCK3=50
   }
 
 }
