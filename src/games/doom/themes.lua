25289abc03bcd4b26c9c02d464c7f3a4e2b55504
GTD-Carthage <gtdcarthage@yahoo.com>
2019-12-22 21:45:38 +0800

    - Normalized test prob value for the new construction frames window set.

--- src/games/doom/themes.lua
+++ src/games/doom/themes.lua
@@ -1216,7 +1216,7 @@ DOOM.THEMES =
       gtd_window_low = 20
       gtd_window_weabdows = 15
       gtd_window_metal_frames = 30
-      gtd_window_construction_frames = 15000
+      gtd_window_construction_frames = 15
     }
 
     fence_groups =
