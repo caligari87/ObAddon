c696e37898b3371c00c018eec00c5c605cb10f58
GTD-Carthage <gtdcarthage@yahoo.com>
2020-01-23 10:39:00 +0800

    - Added new 32-height stair type with dividers and fences.

---
+++ src/games/doom/fabs/stairs/gtd_fancy_stair_32.lua
@@ -0,0 +1,68 @@
+PREFABS.Stair_fancy_32_brown =
+{
+  file   = "stairs/gtd_fancy_stair_32.wad"
+  map    = "MAP01"
+
+  prob   = 5
+  style  = "steepness"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+
+  x_fit  = { 24,104 , 152,232 }
+
+  bound_z1 = 0
+
+  delta_h = 32
+}
+
+PREFABS.Stair_fancy_32_green =
+{
+  template = "Stair_fancy_32_brown"
+
+  flat_CEIL5_2 = "RROCK20"
+  tex_BRONZE5 = "BROWNGRN"
+}
+
+PREFABS.Stair_fancy_32_grey =
+{
+  template = "Stair_fancy_32_brown"
+
+  flat_CEIL5_2 = "FLAT1"
+  tex_BRONZE5 = "GRAY1"
+}
+
+PREFABS.Stair_fancy_32_brown_2X =
+{
+  template = "Stair_fancy_32_brown"
+
+  map = "MAP02"
+
+  seed_h = 2
+}
+
+PREFABS.Stair_fancy_32_green_2X =
+{
+  template = "Stair_fancy_32_brown"
+
+  map = "MAP02"
+
+  seed_h = 2
+
+  flat_CEIL5_2 = "RROCK20"
+  tex_BRONZE5 = "BROWNGRN"
+}
+
+PREFABS.Stair_fancy_32_grey_2X =
+{
+  template = "Stair_fancy_32_brown"
+
+  map = "MAP02"
+
+  seed_h = 2
+
+  flat_CEIL5_2 = "FLAT1"
+  tex_BRONZE5 = "GRAY1"
+}
