87729cea67eda32eaf548b03891ce0b4a06863bf
Scionox <spider100160@yahoo.com>
2020-02-12 17:08:17 +0300

    -New Crate-based fabs

---
+++ src/games/doom/fabs/item/scionox_simple_crates.lua
@@ -0,0 +1,112 @@
+PREFABS.Item_scionox_simple_crates =
+{
+  file  = "item/scionox_simple_crates.wad"
+  where = "point"
+
+  map  = "MAP01"
+
+  prob = 40
+  
+  theme = "!hell"
+
+  height = 128
+  size  = 64
+}
+PREFABS.Item_scionox_simple_crates_2 =
+{
+  template = "Item_scionox_simple_crates"
+  flat_CRATOP2 = "CRATOP1"
+  tex_CRATE1 = "CRATE2"
+}
+PREFABS.Item_scionox_simple_crates_3 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP02"
+  height = 96
+  size  = 96
+}
+PREFABS.Item_scionox_simple_crates_4 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP02"
+  height = 96
+  size  = 96
+  flat_CRATOP2 = "CRATOP1"
+  tex_CRATE1 = "CRATE2"
+}
+PREFABS.Item_scionox_simple_crates_5 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP03"
+  height = 64
+  size  = 48
+  prob = 80
+}
+PREFABS.Item_scionox_simple_crates_6 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP04"
+  size  = 96
+}
+PREFABS.Item_scionox_simple_crates_7 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP04"
+  size  = 96
+  flat_CRATOP2 = "CRATOP1"
+  tex_CRATE1 = "CRATE2"
+}
+PREFABS.Item_scionox_simple_crates_8 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP05"
+}
+PREFABS.Item_scionox_simple_crates_9 =
+{
+  template = "Item_scionox_simple_crates"
+  map = "MAP05"
+  flat_CRATOP2 = "CRATOP1"
+  tex_CRATE1 = "CRATE2"
+}
+PREFABS.Item_scionox_simple_crates_10 =
+{
+  file   = "item/scionox_simple_crates.wad"
+  map    = "MAP06"
+
+  prob   = 50
+  theme  = "!hell"
+  env    = "!cave"
+
+  where  = "seeds"
+  seed_w = 1
+  seed_h = 1
+
+  deep =  16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
+PREFABS.Item_scionox_simple_crates_11 =
+{
+  template = "Item_scionox_simple_crates_10"
+  flat_CRATOP2 = "CRATOP1"
+  tex_CRATE1 = "CRATE2"
+}
+PREFABS.Item_scionox_simple_crates_12 =
+{
+  template = "Item_scionox_simple_crates_10"
+  map = "MAP07"
+}
+PREFABS.Item_scionox_simple_crates_13 =
+{
+  template = "Item_scionox_simple_crates_10"
+  map = "MAP07"
+  flat_CRATOP2 = "CRATOP1"
+  tex_CRATE1 = "CRATE2"
+}
+PREFABS.Item_scionox_simple_crates_14 =
+{
+  template = "Item_scionox_simple_crates_10"
+  map = "MAP08"
+  prob = 100
+}
