79e347dfde9fa10e4044c6dab4aec2d72dd69a31
GTD-Carthage <gtdcarthage@yahoo.com>
2020-02-13 22:11:03 +0800

    - Added pic versions of Scionox's simple crates fabs.

---
+++ src/games/doom/fabs/picture/scionox_pic_simple_crates.lua
@@ -0,0 +1,34 @@
+PREFABS.Pic_simple_crates_1 =
+{
+  file   = "picture/scionox_pic_simple_crates.wad"
+  map    = "MAP01"
+
+  prob   = 25
+  theme = "!hell"
+
+  where  = "seeds"
+  height = 128
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  y_fit = "top"
+}
+
+PREFABS.Pic_simple_crates_2 =
+{
+  template = "Pic_simple_crates_1"
+  map      = "MAP02"
+}
+
+PREFABS.Pic_simple_crates_3 =
+{
+  template = "Pic_simple_crates_1"
+  map      = "MAP03"
+}
