4aec75f1a390a80cdab4e12da2900a26a05c0278
GTD-Carthage <gtdcarthage@yahoo.com>
2020-02-20 10:10:54 +0800

    - Added engine fallbacks for metro keyed/switch-locked joiner pieces.

--- src/games/doom/fabs/hall/metro_k.lua
+++ src/games/doom/fabs/hall/metro_k.lua
@@ -3,6 +3,8 @@ PREFABS.Hallway_metro_locked_red =
   file   = "hall/metro_k.wad"
   map    = "MAP01"
 
+  engine = "zdoom"
+
   kind   = "terminator"
   group  = "metro"
   key    = "k_red"
@@ -51,6 +53,8 @@ PREFABS.Hallway_metro_barred =
   group  = "metro"
   key    = "barred"
 
+  engine = "zdoom"
+
   prob   = 50
 
   where  = "seeds"
@@ -65,4 +69,131 @@ PREFABS.Hallway_metro_barred =
   door_action = "S1_LowerFloor"
 
   sector_1  = { [0]=90, [1]=15 }
+}
+
+-- slopeless engine fallbacks
+
+PREFABS.Hallway_metro_barred_boxy =
+{
+  template = "Hallway_metro_barred"
+  map = "MAP10"
+
+  engine = "any"
+  theme = "!hell"
+
+  prob = 10
+
+  tex_SPCDOOR2 = "SPCDOOR1"
+}
+
+PREFABS.Hallway_metro_barred_boxy_hell =
+{
+  template = "Hallway_metro_barred"
+  map = "MAP10"
+
+  engine = "any"
+  theme = "hell"
+
+  prob = 10
+
+  tex_SPCDOOR1 = "SW1SKIN"
+  tex_SPCDOOR2 = "SW2SKIN"
+}
+
+---- tech/urban versions
+
+PREFABS.Hallway_metro_locked_red_boxy =
+{
+  template = "Hallway_metro_locked_red"
+  map = "MAP11"
+
+  engine = "any"
+  theme = "!hell"
+
+  prob = 10
+}
+
+PREFABS.Hallway_metro_locked_blue_boxy =
+{
+  template = "Hallway_metro_locked_red"
+  map = "MAP11"
+
+  engine = "any"
+  theme = "!hell"
+
+  key = "k_blue"
+  line_33 = 32
+
+  tex_DOORRED = "DOORBLU"
+
+  prob = 10
+}
+
+PREFABS.Hallway_metro_locked_yellow_boxy =
+{
+  template = "Hallway_metro_locked_red"
+  map = "MAP11"
+
+  engine = "any"
+  theme = "!hell"
+
+  key = "k_yellow"
+  line_33 = 34
+
+  tex_DOORRED = "DOORYEL"
+
+  prob = 10
+}
+
+---- hell versions
+
+PREFABS.Hallway_metro_locked_red_boxy_hell =
+{
+  template = "Hallway_metro_locked_red_boxy"
+  map = "MAP11"
+
+  engine = "any"
+  theme = "hell"
+
+  tex_BIGDOOR1 = "BIGDOOR6"
+  tex_DOORRED = "DOORRED2"
+  flat_FLAT23 = "CEIL5_1"
+
+  prob = 10
+}
+
+PREFABS.Hallway_metro_locked_blue_boxy_hell =
+{
+  template = "Hallway_metro_locked_red"
+  map = "MAP11"
+
+  engine = "any"
+  theme = "hell"
+
+  key = "k_blue"
+  line_33 = 32
+
+  tex_DOORRED = "DOORBLU2"
+  tex_BIGDOOR1 = "BIGDOOR6"
+  flat_FLAT23 = "CEIL5_1"
+
+  prob = 10
+}
+
+PREFABS.Hallway_metro_locked_yellow_boxy_hell =
+{
+  template = "Hallway_metro_locked_red"
+  map = "MAP11"
+
+  engine = "any"
+  theme = "hell"
+
+  key = "k_yellow"
+  line_33 = 34
+
+  tex_DOORRED = "DOORYEL2"
+  tex_BIGDOOR1 = "BIGDOOR6"
+  flat_FLAT23 = "CEIL5_1"
+
+  prob = 10
 }
