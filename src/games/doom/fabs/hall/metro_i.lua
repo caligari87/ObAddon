0bb015ac6df0a46eb1f5bc9c5c23ffe8560e8b9a
GTD-Carthage <gtdcarthage@yahoo.com>
2020-02-20 09:18:57 +0800

    - Added engine fallback i-pieces for metro hallway set.

--- src/games/doom/fabs/hall/metro_i.lua
+++ src/games/doom/fabs/hall/metro_i.lua
@@ -3,6 +3,8 @@ PREFABS.Hallway_metro_i =
   file   = "hall/metro_i.wad"
   map    = "MAP01"
 
+  engine = "zdoom"
+
   group  = "metro"
   prob   = 50
 
@@ -20,11 +22,66 @@ PREFABS.Hallway_metro_i_rise =
   template = "Hallway_metro_i"
   map = "MAP02"
 
+  engine = "zdoom"
+
   prob = 15
 
   style = "steepness"
 
   delta_h = 64
+
+  can_flip = true
+}
+
+-- slopeless fallbacks
+PREFABS.Hallway_metro_i_boxy =
+{
+  template = "Hallway_metro_i"
+  map = "MAP10"
+
+  engine = "any"
+
+  prob = 6
+
+  can_flip = true
+}
+
+PREFABS.Hallway_metro_i_boxy_with_door =
+{
+  template = "Hallway_metro_i"
+  map = "MAP11"
+
+  engine = "any"
+
+  prob = 4
+
+  can_flip = true
+}
+
+PREFABS.Hallway_metro_i_boxy_train_crossing =
+{
+  template = "Hallway_metro_i"
+  map = "MAP12"
+
+  theme = "!hell"
+
+  engine = "any"
+
+  prob = 3
+
+  can_flip = true
+}
+
+PREFABS.Hallway_metro_i_boxy_ticketing_office =
+{
+  template = "Hallway_metro_i"
+  map = "MAP13"
+
+  theme = "!hell"
+
+  engine = "any"
+
+  prob = 3
 
   can_flip = true
 }
