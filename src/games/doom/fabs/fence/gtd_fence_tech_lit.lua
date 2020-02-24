f9f5170fddf852ead09c5f60f94b4f32005ebdb1
GTD-Carthage <gtdcarthage@yahoo.com>
2020-02-24 12:54:12 +0800

    - Added EPIC texture variation of the lit fence fab. - Fixed wrong texture assignment on one of the hell accent wall fabs.

--- src/games/doom/fabs/fence/gtd_fence_tech_lit.lua
+++ src/games/doom/fabs/fence/gtd_fence_tech_lit.lua
@@ -16,7 +16,6 @@ PREFABS.Fence_tech_lit =
   bound_z1 = 0
 }
 
-
 PREFABS.Fence_tech_lit_diag =
 {
   file   = "fence/gtd_fence_tech_lit.wad"
@@ -31,4 +30,49 @@ PREFABS.Fence_tech_lit_diag =
   fence_h = 32
 
   bound_z1 = 0
+}
+
+PREFABS.Fence_tech_lit_EPIC =
+{
+  file   = "fence/gtd_fence_tech_lit.wad"
+  map    = "MAP01"
+
+  prob   = 50
+
+  uses_epic_textures = true
+  replaces = "Fence_tech_lit"
+
+  group  = "fence_tech_lit"
+
+  where  = "edge"
+
+  deep   = 16
+  over   = 16
+
+  fence_h  = 32
+  bound_z1 = 0
+
+  tex_MIDSPACE = "MIDSPAC5"
+}
+
+
+PREFABS.Fence_tech_lit_diag_EPIC =
+{
+  file   = "fence/gtd_fence_tech_lit.wad"
+  map    = "MAP02"
+
+  prob   = 50
+
+  uses_epic_textures = true
+  replaces = "Fence_tech_lit_diag"
+
+  group  = "fence_tech_lit"
+
+  where  = "diagonal"
+
+  fence_h = 32
+
+  bound_z1 = 0
+
+  tex_MIDSPACE = "MIDSPAC5"
 }
