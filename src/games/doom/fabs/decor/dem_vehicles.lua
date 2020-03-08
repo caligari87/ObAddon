f991eef01ca15dcb578ede15b32cc71d75ce32ec
GTD-Carthage <gtdcarthage@yahoo.com>
2020-03-09 07:53:53 +0800

    - Added Demios's forklift fab!

--- src/games/doom/fabs/decor/dem_vehicles.lua
+++ src/games/doom/fabs/decor/dem_vehicles.lua
@@ -27,3 +27,267 @@ PREFABS.Decor_apc =
 
   sink_mode = "never"
 }
+
+PREFABS.Decor_forklift1T =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP02"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "tech"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift2T =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP03"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "tech"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift3T =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP04"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "tech"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift4T =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP05"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "tech"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift5T =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP06"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "tech"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+
+  thing_22 =
+  {
+    skull_rock = 50
+    pool_blood_1  = 50
+    green_pillar = 50
+    green_column = 50
+    red_pillar = 50
+    red_column = 50
+    dead_imp = 50
+    dead_demon =50
+    dead_caco =50
+  }
+}
+
+PREFABS.Decor_forklift1U =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP02"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "urban"
+
+  uses_epic_textures = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift2U =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP03"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "urban"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift3U =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP04"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "urban"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift4U =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP05"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "urban"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+}
+
+PREFABS.Decor_forklift5U =
+{
+  file   = "decor/dem_vehicles.wad"
+  map    = "MAP06"
+
+  engine = "zdoom"
+
+  prob   = 4500
+  theme  = "urban"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where  = "point"
+  height = 128
+  size   = 128
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  sink_mode = "never"
+
+  thing_22 =
+  {
+    skull_rock = 50
+    pool_blood_1  = 50
+    green_pillar = 50
+    green_column = 50
+    red_pillar = 50
+    red_column = 50
+    dead_imp = 50
+    dead_demon =50
+    dead_caco =50
+  }
+}
