2f4843e2192aab938962376649c5a8fb15b64288
GTD-Carthage <gtdcarthage@yahoo.com>
2020-03-12 18:14:07 +0800

    - Added new fab: a key dispenser!

---
+++ src/games/doom/fabs/item/gtd_item_key_dispenser.lua
@@ -0,0 +1,22 @@
+PREFABS.Item_gtd_key_dispenser =
+{
+  file   = "item/gtd_item_key_dispenser.wad"
+  map = "MAP01"
+
+  rank = 2
+  prob   = 100
+  theme  = "!hell"
+  env    = "!cave"
+
+  item_kind = "key"
+
+  where  = "seeds"
+  seed_w = 2
+  seed_h = 1
+
+  deep =  16
+  over = -16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
