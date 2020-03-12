54b0d242d66d69926109d648d3e99300e17bd767
Scionox <spider100160@yahoo.com>
2020-03-12 20:26:36 +0300

    -Added a few nature-based item fabs

---
+++ src/games/doom/fabs/item/scionox_nature_simple.lua
@@ -0,0 +1,44 @@
+PREFABS.Item_scionox_nature_simple =
+{
+  file  = "item/scionox_nature_simple.wad"
+  where = "point"
+
+  map  = "MAP01"
+  env  = "cave"
+
+  size = 48
+  prob = 75
+}
+
+PREFABS.Item_scionox_nature_simple_2 =
+{
+  template = "Item_scionox_nature_simple"
+  
+  env  = "nature"
+
+  size = 80
+  height = 152
+
+  map = "MAP02"
+}
+
+PREFABS.Item_scionox_nature_simple_3 =
+{
+  template = "Item_scionox_nature_simple"
+
+  size = 80
+  height = 128
+
+  map = "MAP03"
+}
+
+PREFABS.Item_scionox_nature_simple_4 =
+{
+  template = "Item_scionox_nature_simple"
+  
+  env  = "nature"
+
+  size = 72
+
+  map = "MAP04"
+}
