d34a218dacacea19dc6b278f8ab504e4111c8532
GTD-Carthage <gtdcarthage@yahoo.com>
2020-03-15 13:43:19 +0800

    - Redid texture assignments and probability settings on some hell wall pieces.

--- src/games/doom/fabs/wall/gtd_wall_hell_sharp_alcoves.lua
+++ src/games/doom/fabs/wall/gtd_wall_hell_sharp_alcoves.lua
@@ -1,10 +1,14 @@
-PREFABS.Wall_hell_sharp_alcoves_single =
+-- pencil-shaped wall alcoves of sorts
+
+-- red version
+
+PREFABS.Wall_hell_sharp_alcoves_single_red = --#
 {
   file   = "wall/gtd_wall_hell_sharp_alcoves.wad"
   map    = "MAP01"
 
   prob   = 50
-  skip_prob = 66
+  skip_prob = 90.75
 
   theme  = "hell"
   env = "building"
@@ -17,31 +21,120 @@ PREFABS.Wall_hell_sharp_alcoves_single =
   bound_z2 = 128
 
   z_fit  = { 32,40 }
-
-  tex_FIRELAVA =
-  {
-    FIRELAVA=1
-    FIREBLU1=1
-    FIREMAG1=1
-    CRACKLE2=1
-    CRACKLE4=1
-    ROCKRED1=1
-    SKIN2=1
-  }
-
-  thing_35 =
-  {
-    evil_eye=1
-    candelabra=1
-    blue_torch=1
-    green_torch=1
-    red_torch=1
-    skull_cairn=1
-  }
 }
 
-PREFABS.Wall_hell_sharp_alcoves_double =
+PREFABS.Wall_hell_sharp_alcoves_double_red = --#
 {
   template = "Wall_hell_sharp_alcoves_single"
   map      = "MAP02"
+}
+
+-- blue version
+
+PREFABS.Wall_hell_sharp_alcoves_blue = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP01"
+
+  tex_FIREMAG1 = "COMPBLUE"
+  thing_46 = 44
+}
+
+PREFABS.Wall_hell_sharp_alcoves_double_blue = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP02"
+
+  tex_FIREMAG1 = "COMPBLUE"
+  thing_46 = 44
+}
+
+-- green version
+
+PREFABS.Wall_hell_sharp_alcoves_green = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP01"
+
+  tex_FIREMAG1 = "ZIMMER7"
+  thing_46 = 45
+}
+
+PREFABS.Wall_hell_sharp_alcoves_double_green = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP02"
+
+  tex_FIREMAG1 = "ZIMMER7"
+  thing_46 = 45
+}
+
+-- gold
+
+PREFABS.Wall_hell_sharp_alcoves_gold = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP01"
+
+  tex_FIREMAG1 = "CRACKLE4"
+  thing_46 = 35
+}
+
+PREFABS.Wall_hell_sharp_alcoves_double_gold = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP02"
+
+  tex_FIREMAG1 = "CRACKLE4"
+  thing_46 = 35
+}
+
+-- EPIC versions start here:
+
+-- white
+
+PREFABS.Wall_hell_sharp_alcoves_white = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP01"
+
+  uses_epic_textures = true
+
+  tex_FIREMAG1 = "ICEFALL"
+  thing_46 = 86
+}
+
+PREFABS.Wall_hell_sharp_alcoves_double_white = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP02"
+
+  uses_epic_textures = true
+
+  tex_FIREMAG1 = "ICEFALL"
+  thing_46 = 86
+}
+
+-- black
+
+PREFABS.Wall_hell_sharp_alcoves_black = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP01"
+
+  uses_epic_textures = true
+
+  tex_FIREMAG1 = "FIREBLK1"
+  thing_46 = 41
+}
+
+PREFABS.Wall_hell_sharp_alcoves_double_black = --#
+{
+  template = "Wall_hell_sharp_alcoves_single_red"
+  map = "MAP02"
+
+  uses_epic_textures = true
+
+  tex_FIREMAG1 = "FIREBLK1"
+  thing_46 = 41
 }
