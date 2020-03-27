e0a08165b5f3c4a9670ac4732b9fa0fa36a7096e
GTD-Carthage <gtdcarthage@yahoo.com>
2020-03-27 15:53:09 +0800

    - Added new techy machine pic fab using Epic textures.

---
+++ src/games/doom/fabs/picture/gtd_pic_tech_wallmachines_EPIC.lua
@@ -0,0 +1,24 @@
+PREFABS.Pic_gtd_tech_core_thing =
+{
+  file   = "picture/gtd_pic_tech_wallmachines_EPIC.wad"
+  map    = "MAP01"
+
+  prob   = 25
+  theme = "!hell"
+
+  uses_epic_textures = true
+
+  where  = "seeds"
+  height = 128
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "frame"
+  y_fit = "top"
+}
