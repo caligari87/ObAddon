7b39caf1085c8ea114eaa5a1542a1fbfdd08e79d
GTD-Carthage <gtdcarthage@yahoo.com>
2020-04-22 11:05:14 +0800

    - Added more of Demios's new nature fabs.

--- src/games/doom/fabs/picture/dem_pic_nature.lua
+++ src/games/doom/fabs/picture/dem_pic_nature.lua
@@ -5,7 +5,6 @@ PREFABS.Pic_dem_cavein1 =
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP01"
 
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -43,7 +42,6 @@ PREFABS.Pic_dem_cavein4 =
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP04"
 
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -81,8 +79,6 @@ PREFABS.Pic_dem_cavein7 =
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP07"
 
-  engine = "zdoom"
-
   prob   = 100
   env = "cave"
 
@@ -118,8 +114,6 @@ PREFABS.Pic_dem_cavein10 =
 {
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP10"
-
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -158,7 +152,6 @@ PREFABS.Pic_dem_cavein13 =
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP13"
 
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -198,13 +191,13 @@ PREFABS.Pic_dem_caveind1 =
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP16"
 
-  engine = "zdoom"
-
   prob   = 100
   env = "cave"
 
   where  = "seeds"
   height = 128
+
+  liquid = true
 
   seed_w = 3
   seed_h = 2
@@ -219,7 +212,7 @@ PREFABS.Pic_dem_caveind1 =
 
   tex_MIDVINE1 = {
     MIDVINE1=50, MIDVINE2=50
-  }
+    }
 
 uses_epic_textures = true
 
@@ -229,20 +222,20 @@ PREFABS.Pic_dem_caveind2 =
 {
   template  = "Pic_dem_caveind1"
   map    = "MAP17"
+  liquid = false
 }
 
 PREFABS.Pic_dem_caveind3 =
 {
   template  = "Pic_dem_caveind1"
   map    = "MAP18"
+  liquid = true
 }
 
 PREFABS.Pic_dem_caveind4 =
 {
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP19"
-
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -263,7 +256,7 @@ PREFABS.Pic_dem_caveind4 =
 
   tex_MIDVINE1 = {
     MIDVINE1=50, MIDVINE2=50
-  }
+    }
 
 uses_epic_textures = true
 
@@ -273,20 +266,20 @@ PREFABS.Pic_dem_caveind5 =
 {
   template  = "Pic_dem_caveind4"
   map    = "MAP20"
+  liquid = true
 }
 
 PREFABS.Pic_dem_caveind6 =
 {
   template  = "Pic_dem_caveind4"
   map    = "MAP21"
+  liquid = true
 }
 
 PREFABS.Pic_dem_caveind7 =
 {
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP22"
-
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -299,6 +292,7 @@ PREFABS.Pic_dem_caveind7 =
 
   deep = 16
   over = -16
+  liquid = true
 
   bound_z1 = 0
   bound_z2 = 128
@@ -307,7 +301,7 @@ PREFABS.Pic_dem_caveind7 =
 
   tex_MIDVINE1 = {
     MIDVINE1=50, MIDVINE2=50
-  }
+    }
 
 uses_epic_textures = true
 
@@ -317,20 +311,20 @@ PREFABS.Pic_dem_caveind8 =
 {
   template  = "Pic_dem_caveind7"
   map    = "MAP23"
+  liquid = true
 }
 
 PREFABS.Pic_dem_caveind9 =
 {
   template  = "Pic_dem_caveind7"
   map    = "MAP24"
+  liquid = false
 }
 
 PREFABS.Pic_dem_caveind10 =
 {
   file   = "picture/dem_pic_nature.wad"
   map    = "MAP25"
-
-  engine = "zdoom"
 
   prob   = 100
   env = "cave"
@@ -344,6 +338,7 @@ PREFABS.Pic_dem_caveind10 =
 
   deep = 16
   over = -16
+  liquid = true
 
   bound_z1 = 0
   bound_z2 = 128
@@ -352,7 +347,7 @@ PREFABS.Pic_dem_caveind10 =
 
   tex_MIDVINE1 = {
     MIDVINE1=50, MIDVINE2=50
-  }
+    }
 
 uses_epic_textures = true
 
@@ -369,3 +364,434 @@ PREFABS.Pic_dem_caveind12 =
   template  = "Pic_dem_caveind10"
   map    = "MAP27"
 }
+
+---- outdoor nooks ----
+
+PREFABS.Pic_dem_nook1 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP28"
+
+  prob   = 100
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 3
+  seed_h = 2
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "stretch"
+  z_fit = "stretch"
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nook2 =
+{
+  template  = "Pic_dem_nook1"
+  map    = "MAP29"
+}
+
+PREFABS.Pic_dem_nook3 =
+{
+  template  = "Pic_dem_nook1"
+  map    = "MAP30"
+}
+
+PREFABS.Pic_dem_nook4 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP31"
+
+  prob   = 100
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 3
+  seed_h = 1
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "stretch"
+  z_fit = "stretch"
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nook5 =
+{
+  template  = "Pic_dem_nook4"
+  map    = "MAP32"
+}
+
+PREFABS.Pic_dem_nook6 =
+{
+  template  = "Pic_dem_nook4"
+  map    = "MAP33"
+}
+
+PREFABS.Pic_dem_nook7 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP34"
+
+  prob   = 100
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "stretch"
+  z_fit = "stretch"
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nook8 =
+{
+  template  = "Pic_dem_nook7"
+  map    = "MAP35"
+}
+
+PREFABS.Pic_dem_nook9 =
+{
+  template  = "Pic_dem_nook7"
+  map    = "MAP36"
+}
+
+PREFABS.Pic_dem_nook10 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP37"
+
+  prob   = 100
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "stretch"
+  z_fit = "stretch"
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nook11 =
+{
+  template  = "Pic_dem_nook10"
+  map    = "MAP38"
+}
+
+PREFABS.Pic_dem_nook12 =
+{
+  template  = "Pic_dem_nook10"
+  map    = "MAP39"
+}
+
+
+
+---- outdoor nooks with deco ----
+
+PREFABS.Pic_dem_nookd1 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP40"
+
+  prob   = 100
+  theme = "!hell"
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 3
+  seed_h = 2
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit = { 56,62 }
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nookd2 =
+{
+  template  = "Pic_dem_nookd1"
+  map    = "MAP41"
+  theme = "any"
+  liquid = true
+}
+
+PREFABS.Pic_dem_nookd3 =
+{
+  template  = "Pic_dem_nookd1"
+  map    = "MAP42"
+  theme = "!hell"
+}
+
+PREFABS.Pic_dem_nookd4 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP43"
+
+  prob   = 100
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 3
+  seed_h = 1
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit = "stretch"
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nookd5 =
+{
+  template  = "Pic_dem_nookd4"
+  map    = "MAP44"
+}
+
+PREFABS.Pic_dem_nookd6 =
+{
+  template  = "Pic_dem_nookd4"
+  map    = "MAP45"
+  z_fit = { 56,62 }
+  liquid = true
+}
+
+PREFABS.Pic_dem_nookd7 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP46"
+
+  prob   = 100
+  env = "nature"
+  theme = "!hell"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 2
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "stretch"
+  z_fit = { 56,64 }
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nookd8 =
+{
+  template  = "Pic_dem_nookd7"
+  map    = "MAP47"
+  z_fit = "stretch"
+  theme = "any"
+  liquid = true
+}
+
+PREFABS.Pic_dem_nookd9 =
+{
+  template  = "Pic_dem_nookd7"
+  map    = "MAP48"
+  z_fit = { 40,56 }
+  theme = "any"
+}
+
+PREFABS.Pic_dem_nookd10 =
+{
+  file   = "picture/dem_pic_nature.wad"
+  map    = "MAP49"
+
+
+rank = 3
+
+  prob   = 100
+  env = "nature"
+
+  group = "natural_walls"
+
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep = 16
+  over = -16
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  x_fit = "stretch"
+  z_fit = "stretch"
+
+
+  thing_43 =
+  {
+    [43] = 50
+    [0] = 50
+  }
+
+  thing_54 =
+  {
+    [54] = 50
+    [43] = 25
+    [0] = 50
+  }
+}
+
+PREFABS.Pic_dem_nookd11 =
+{
+  template  = "Pic_dem_nookd10"
+  map    = "MAP50"
+  theme = "!hell"
+  z_fit = { 56,64 }
+}
+
+PREFABS.Pic_dem_nookd12 =
+{
+  template  = "Pic_dem_nookd10"
+  map    = "MAP51"
+  liquid = true
+}
+
