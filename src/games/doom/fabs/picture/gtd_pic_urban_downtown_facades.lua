ce334b2b29b6d90d0fc022184663cf7059d36b72
GTD-Carthage <gtdcarthage@yahoo.com>
2020-04-24 11:08:19 +0800

    - Added some new downtown pics for urban.

--- src/games/doom/fabs/picture/gtd_pic_urban_downtown_facades.lua
+++ src/games/doom/fabs/picture/gtd_pic_urban_downtown_facades.lua
@@ -13,7 +13,6 @@ PREFABS.Pic_urban_downtown_facade1_vanilla =
 
   where  = "seeds"
   deep   = 16
-  over   = -16
 
   height = 256
 
@@ -100,7 +99,6 @@ PREFABS.Pic_urban_downtown_facade1_EPIC =
 
   where  = "seeds"
   deep   = 16
-  over   = -16
 
   height = 256
 
@@ -312,7 +310,6 @@ PREFABS.Pic_urban_downtown_facade3_EPIC =
 
   where  = "seeds"
   deep   = 16
-  over   = -16
 
   height = 256
 
@@ -337,66 +334,6 @@ PREFABS.Pic_urban_downtown_facade3_EPIC =
     CITY12 = 25
     CITY13 = 25
     CITY14 = 25
-  }
-
-  flat_TLITE6_6 =
-  {
-    TLITE5_2 = 25
-    TLITE5_3 = 25
-    TLITE6_5 = 25
-    TLITE6_6 = 25
-    TLITE65B = 25
-    TLITE65G = 25
-    TLITE65O = 25
-    TLITE65W = 25
-    TLITE65Y = 25
-    GLITE04 = 50
-    GLITE06 = 50
-    GLITE07 = 50
-    GLITE08 = 50
-    GLITE09 = 50
-    LIGHTS1 = 50
-    LIGHTS2 = 50
-    LIGHTS3 = 50
-    LIGHTS4 = 50
-  }
-
-  -- textures for the top half of the 'door' area
-  tex_MODWALL3 =
-  {
-    MODWALL2 = 50
-    MODWALL3 = 50
-    MODWALL4 = 50
-    BLAKWAL1 = 50
-    BLAKWAL2 = 50
-    TEKWALL8 = 15
-    TEKWALL9 = 15
-    TEKWALLA = 15
-    TEKWALLB = 15
-    TEKWALLC = 15
-    TEKWALLD = 15
-    TEKWALLE = 15
-    COLLITE1 = 50
-    COLLITE2 = 50
-    COLLITE3 = 50
-  }
-
-  -- textures for the bottom half (and all of the 'doors' in facade2)
-  tex_MODWALL4 =
-  {
-    MODWALL2 = 50
-    MODWALL3 = 50
-    MODWALL4 = 50
-    BLAKWAL1 = 50
-    BLAKWAL2 = 50
-    STEP1 = 50
-    STEP2 = 50
-    STEP3 = 50
-    STEP4 = 50
-    STEP5 = 50
-    STEPLAD1 = 50
-    URBAN6 = 175
-    URBAN8 = 175
   }
 
   tex_STEP4 =
@@ -426,4 +363,30 @@ PREFABS.Pic_urban_downtown_facade_EPIC_destroyed =
   over = 0
 
   x_fit = "frame"
+}
+
+PREFABS.Pic_urban_downtown_facade_fenced_out =
+{
+  template = "Pic_urban_downtown_facade3_EPIC"
+  map = "MAP05"
+
+  prob = 200
+
+  y_fit = { 32,40 }
+  z_fit = "top"
+}
+
+PREFABS.Pic_urban_downtown_facade_alley =
+{
+  template = "Pic_urban_downtown_facade3_EPIC"
+  map = "MAP06"
+
+  height = 160
+
+  seed_h = 2
+
+  prob = 200
+
+  y_fit = { 32,40 }
+  z_fit = { 72,80 }
 }
