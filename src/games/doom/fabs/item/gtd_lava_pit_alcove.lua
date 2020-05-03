ff449abb83eeebb145a05a7d7e4a79c08cd8f98b
GTD-Carthage <gtdcarthage@yahoo.com>
2020-05-03 11:23:24 +0800

    - Fixed issue with switched floor line special having no tags in lava item alcove fab.

--- src/games/doom/fabs/item/gtd_lava_pit_alcove.lua
+++ src/games/doom/fabs/item/gtd_lava_pit_alcove.lua
@@ -25,21 +25,9 @@ PREFABS.Item_lava_pit_alcove =
 -- Lava trap
 PREFABS.Item_lava_pit_alcove_trap =
 {
-  file   = "item/gtd_lava_pit_alcove.wad"
-  map    = "MAP03"
+  template = "Item_lava_pit_alcove"
+  map    = "MAP02"
 
   prob   = 25
-  theme  = "!hell"
   style  = "traps"
-
-  where  = "seeds"
-  seed_w = 3
-  seed_h = 2
-
-  height = 192
-  deep =  16
-  over = -16
-
-  x_fit = "frame"
-  y_fit = "top"
 }
