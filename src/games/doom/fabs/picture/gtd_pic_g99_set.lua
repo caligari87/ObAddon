ac9776f09a24ecc040c6763159d7e1b3e4979a93
GTD-Carthage <gtdcarthage@yahoo.com>
2020-05-14 22:05:42 +0800

    - Reduced filesize footprint of various fabs. - Added new picture fab for 'Gothic 99' wall set.

---
+++ src/games/doom/fabs/picture/gtd_pic_g99_set.lua
@@ -0,0 +1,22 @@
+PREFABS.Pic_gtd_g99_1 =
+{
+  file   = "picture/gtd_pic_g99_set.wad"
+  map    = "MAP01"
+
+  prob  = 50
+  theme = "hell"
+
+  group = "gtd_g99"
+
+  uses_epic_textures = true
+
+  where  = "seeds"
+  seed_w = 2
+  seed_h = 1
+
+  height = 160
+  deep   =  16
+
+  x_fit = "frame"
+  y_fit = "top"
+}
