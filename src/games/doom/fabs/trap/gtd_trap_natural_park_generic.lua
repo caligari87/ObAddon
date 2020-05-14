73ade58cbcf32849dce5482153f5bcdf9d8412bb
GTD-Carthage <gtdcarthage@yahoo.com>
2020-05-14 15:14:43 +0800

    - Added a fallback for closet traps in naturalistic parks.

---
+++ src/games/doom/fabs/trap/gtd_trap_natural_park_generic.lua
@@ -0,0 +1,24 @@
+PREFABS.Trap_naturalistic_park_generic =
+{
+  file   = "trap/gtd_trap_natural_park_generic.wad"
+
+  prob   = 50
+
+  where  = "seeds"
+  shape  = "U"
+
+  group = "natural_walls"
+
+  seed_w = 2
+  seed_h = 1
+
+  over   = -16
+
+  x_fit = "stretch"
+  y_fit = top
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  tag_1 = "?trap_tag"
+}
