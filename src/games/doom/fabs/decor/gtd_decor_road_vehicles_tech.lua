56623ffe4a589799e8f988e87d4adda13df834a1
GTD-Carthage <gtdcarthage@yahoo.com>
2020-05-19 12:59:36 +0800

    - Added a new road vehicle for tech roads and outdoors.

---
+++ src/games/doom/fabs/decor/gtd_decor_road_vehicles_tech.lua
@@ -0,0 +1,21 @@
+PREFABS.Decor_road_vehicle_tractor_thing =
+{
+  file = "decor/gtd_decor_road_vehicles_tech.wad"
+  map = "MAP01"
+
+  prob = 3500
+  theme = "tech"
+  env = "outdoor"
+
+  uses_epic_textures = true
+
+  can_be_on_roads = true
+
+  where = "point"
+  size = 160
+  height = 88
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+}
