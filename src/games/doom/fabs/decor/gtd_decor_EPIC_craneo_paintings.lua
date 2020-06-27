f18d845d153b4137b563a513d9d72fddd205226d
GTD-Carthage <gtdcarthage@yahoo.com>
2020-06-27 12:25:28 +0800

    - Addded point decor for Craneo's painting wall set.

---
+++ src/games/doom/fabs/decor/gtd_decor_EPIC_craneo_paintings.lua
@@ -0,0 +1,16 @@
+PREFABS.Decor_EPIC_craneo_painting_point =
+{
+  file   = "decor/gtd_decor_EPIC_craneo_paintings.wad"
+  map    = "MAP01"
+
+  group = "gtd_epic_craneo_paintings"
+
+  uses_epic_textures = true
+
+  prob   = 5000
+
+  where  = "point"
+  size   = 96
+
+  bound_z1 = 0
+}
