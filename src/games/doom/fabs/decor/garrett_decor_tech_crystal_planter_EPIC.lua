85b49de6e6d4096ada1be3f6aad398cce2e0c57a
GTD-Carthage <gtdcarthage@yahoo.com>
2020-07-02 16:28:01 +0800

    - Added more variants of Garrett's hell crystal decor fab.

---
+++ src/games/doom/fabs/decor/garrett_decor_tech_crystal_planter_EPIC.lua
@@ -0,0 +1,47 @@
+PREFABS.Decor_garrett_hell_crystal_analyser_thing_EPIC =
+{
+  file   = "decor/garrett_decor_tech_crystal_planter_EPIC.wad"
+  map = "MAP01"
+
+  prob   = 5000
+  theme  = "tech"
+  env    = "building"
+
+  uses_epic_textures = true
+
+  where  = "point"
+
+  height = 192
+  size   = 96
+
+  bound_z1 = 0
+  bound_z2 = 192
+
+  z_fit = { 48,136 }
+}
+
+PREFABS.Decor_gtd_hell_crystal_analyser_thing_2_EPIC =
+{
+  template = "Decor_garrett_hell_crystal_analyser_thing_EPIC"
+  map = "MAP02"
+
+  height = 128
+  size = 104
+
+  bound_z2 = 128
+
+  z_fit = "top"
+}
+
+PREFABS.Decor_gtd_hell_crystal_analyser_thing_3_EPIC =
+{
+  template = "Decor_garrett_hell_crystal_analyser_thing_EPIC"
+  map = "MAP03"
+
+  height = 128
+  size = 112
+
+  bound_z1 = 128
+
+  z_fit = "top"
+}
