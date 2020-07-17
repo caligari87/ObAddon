76f87df07010feccd24b6d93d1c0cfd248164a7b
GTD-Carthage <gtdcarthage@yahoo.com>
2020-07-17 12:17:33 +0800

    - Added missing keyed L joiner for Heretic.

---
+++ src/games/heretic/fabs/joiner/gtd_basic_keyed_L.lua
@@ -0,0 +1,33 @@
+PREFABS.Joiner_gtd_basic_keyed_curve_green =
+{
+  file   = "joiner/gtd_basic_keyed_L.wad"
+  map    = "MAP01"
+
+  prob   = 30
+
+  key    = "k_green"
+
+  where  = "seeds"
+  shape  = "L"
+
+  seed_w = 2
+  seed_h = 2
+}
+
+PREFABS.Joiner_gtd_basic_keyed_curve_blue =
+{
+  template = "Joiner_gtd_basic_keyed_curve_green"
+
+  key = "k_blue"
+
+  line_33 = 32
+}
+
+PREFABS.Joiner_gtd_basic_keyed_curve_yell =
+{
+  template = "Joiner_gtd_basic_keyed_curve_green"
+
+  key = "k_yellow"
+
+  line_33 = 34
+}
