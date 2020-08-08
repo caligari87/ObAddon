fe8593fac8b474c83476b7dcfc6f08beb7be74af
GTD-Carthage <gtdcarthage@yahoo.com>
2020-08-08 14:14:45 +0800

    - Increased prob on some theme-unique secret fabs. - Simplified some details on wall fabs for hell/gothic keyword themes. - Added pic for teklitecomp indoor wall set.

--- src/games/doom/fabs/item/scionox_secrets_urban.lua
+++ src/games/doom/fabs/item/scionox_secrets_urban.lua
@@ -1,16 +1,16 @@
 --Based on gtd_pic_urban_generic_frontages
 PREFABS.Item_generic_frontage_secret =
 {
-  file   = "item/scionox_secrets_urban.wad"
-  map    = "MAP01"
+  file = "item/scionox_secrets_urban.wad"
+  map = "MAP01"
 
-  prob   = 25
+  prob = 40
   theme = "urban"
 
   env = "!nature"
 
-  where  = "seeds"
-  key    = "secret"
+  where = "seeds"
+  key = "secret"
   height = 128
 
   seed_w = 3
@@ -19,7 +19,7 @@ PREFABS.Item_generic_frontage_secret =
   bound_z1 = 0
   bound_z2 = 128
 
-  deep   =  16
+  deep = 16
 
   x_fit = "frame"
   y_fit = "top"
@@ -42,18 +42,18 @@ PREFABS.Item_generic_frontage_secret =
 
 PREFABS.Item_generic_frontage_secret_2 =
 {
-  file   = "item/scionox_secrets_urban.wad"
-  map    = "MAP02"
+  file = "item/scionox_secrets_urban.wad"
+  map = "MAP02"
 
-  prob   = 15
+  prob = 15
   theme = "urban"
 
   engine = "zdoom"
 
   env = "!nature"
 
-  where  = "seeds"
-  key    = "secret"
+  where = "seeds"
+  key = "secret"
   height = 128
 
   seed_w = 2
@@ -70,16 +70,16 @@ PREFABS.Item_generic_frontage_secret_2 =
 
 PREFABS.Item_generic_frontage_secret_3 =
 {
-  file   = "item/scionox_secrets_urban.wad"
-  map    = "MAP03"
+  file = "item/scionox_secrets_urban.wad"
+  map = "MAP03"
 
-  prob   = 25
+  prob = 40
   theme = "urban"
 
   env = "!nature"
 
-  where  = "seeds"
-  key    = "secret"
+  where = "seeds"
+  key = "secret"
   height = 128
 
   seed_w = 3
@@ -88,7 +88,7 @@ PREFABS.Item_generic_frontage_secret_3 =
   bound_z1 = -96
   bound_z2 = 128
 
-  deep   =  16
+  deep = 16
 
   x_fit = "frame"
   y_fit = "top"
@@ -98,10 +98,10 @@ PREFABS.Item_generic_frontage_secret_3 =
 
 PREFABS.Item_commercial_frontage_secret =
 {
-  file   = "item/scionox_secrets_urban.wad"
-  map    = "MAP04"
+  file = "item/scionox_secrets_urban.wad"
+  map = "MAP04"
 
-  prob   = 15
+  prob = 15
   theme = "urban"
 
   env = "!nature"
@@ -116,7 +116,7 @@ PREFABS.Item_commercial_frontage_secret =
   bound_z1 = 0
   bound_z2 = 128
 
-  deep   =  16
+  deep = 16
 
   x_fit = "frame"
   y_fit = "top"
@@ -126,50 +126,63 @@ PREFABS.Item_commercial_frontage_secret_2 =
 {
   template = "Item_commercial_frontage_secret"
   map    = "MAP05"
+
   seed_w = 3
-  prob   = 25
+
+  prob = 40
 }
 
 PREFABS.Item_commercial_frontage_secret_3 =
 {
   template = "Item_commercial_frontage_secret"
   map    = "MAP06"
+
   seed_w = 3
-  prob   = 25
+
+  prob = 40
 }
 
 PREFABS.Item_commercial_frontage_secret_4 =
 {
   template = "Item_commercial_frontage_secret"
-  map    = "MAP07"
+  map = "MAP07"
+
+  prob = 20
+
   seed_w = 2
   seed_h = 2
-  prob   = 20
 }
 
 PREFABS.Item_commercial_frontage_secret_5 =
 {
   template = "Item_commercial_frontage_secret"
-  map    = "MAP08"
+  map = "MAP08"
+
+  prob = 40
+
   engine = "zdoom"
   seed_w = 3
-  prob   = 25
 }
 
 PREFABS.Item_commercial_frontage_secret_6 =
 {
   template = "Item_commercial_frontage_secret"
-  map    = "MAP09"
+  map = "MAP09"
+
+  prob = 35
+
   seed_w = 3
   seed_h = 1
-  prob   = 20
 }
 
 PREFABS.Item_commercial_frontage_secret_7 =
 {
   template = "Item_commercial_frontage_secret"
-  map    = "MAP10"
+  map = "MAP10"
+
+  prob = 40
+
   engine = "zdoom"
   seed_w = 3
-  prob   = 25
+
 }
