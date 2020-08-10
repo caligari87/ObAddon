6bd27c2a93e2f242414395c27e69eb858ee60381
GTD-Carthage <gtdcarthage@yahoo.com>
2020-08-10 18:42:49 +0800

    - Added uses_epic_texture properties to Demios's teleporter fabs and are now templated from a single def.

--- src/games/doom/fabs/teleporter/dem_tp.lua
+++ src/games/doom/fabs/teleporter/dem_tp.lua
@@ -4,12 +4,14 @@
 
 PREFABS.Dem_teleports1 =
 {
-  file   = "teleporter/dem_tp.wad"
-  map    = "MAP02"
+  file = "teleporter/dem_tp.wad"
+  map = "MAP02"
 
-  prob   = 125
+  uses_epic_textures = true
 
-  where  = "point"
+  prob = 50
+
+  where = "point"
 
   tag_1 = "?out_tag"
   tag_2 = "?in_tag"
@@ -19,54 +21,31 @@ PREFABS.Dem_teleports1 =
 
 PREFABS.Dem_teleports2 =
 {
-  file   = "teleporter/dem_tp.wad"
-  map    = "MAP02"
-
-  prob   = 125
-
-  where  = "point"
-
-  tag_1 = "?out_tag"
-  tag_2 = "?in_tag"
-
-  sector_8  = { [8]=60, [2]=10, [3]=10, [17]=10, [21]=10 }
+  template = "Dem_teleports1"
+  map = "MAP02"
 }
 
 --- Lill tribute to Turok 1 ---
 
 PREFABS.Dem_teleports3 =
 {
-  file   = "teleporter/dem_tp.wad"
-  map    = "MAP03"
-
-  prob   = 125
-
-  where  = "point"
-
-  tag_1 = "?out_tag"
-  tag_2 = "?in_tag"
-
+  template = "Dem_teleports1"
+  map = "MAP03"
 }
 
 PREFABS.Dem_teleports4 =
 {
-  file   = "teleporter/dem_tp.wad"
-  map    = "MAP04"
+  template = "Dem_teleports1"
+  map = "MAP04"
 
-  prob   = 125
-
-  where  = "seeds"
-  env   = "nature"
+  where = "seeds"
+  env = "nature"
 
   seed_w = 2
   seed_h = 2
 
-  deep  =  16
+  deep = 16
 
   x_fit = "frame"
   y_fit = "top"
-
-  tag_1 = "?out_tag"
-  tag_2 = "?in_tag"
-
 }
--- src/games/doom/fabs/teleporter/garrett_hell_portal.lua
+++ src/games/doom/fabs/teleporter/garrett_hell_portal.lua
@@ -1,18 +1,18 @@
 PREFABS.Hell_garrett_portal =
 {
-  file   = "teleporter/garrett_hell_portal.wad"
-  map    = "MAP01"
-  prob   = 50
+  file = "teleporter/garrett_hell_portal.wad"
+  map = "MAP01"
+  prob = 50
 
-  theme  = "hell"
-  where  = "seeds"
-  env    = "!outdoor"
+  theme = "hell"
+  where = "seeds"
+  env = "!outdoor"
 
   seed_w = 2
   seed_h = 2
 
-  deep  =  16
-  over  = -16
+  deep = 16
+  over = -16
 
   x_fit = "frame"
   y_fit = "top"
