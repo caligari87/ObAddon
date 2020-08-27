7edec7e06a9c5cca3fe315ffe5a40a7fb5893b26
Scionox <spider100160@yahoo.com>
2020-08-27 21:32:41 +0400

    -AI Marine closets: moved from attic and added prefab def

---
+++ src/games/doom/fabs/quest/gtd_cage_teleporter_m.lua
@@ -0,0 +1,34 @@
+PREFABS.Quest_marine_teleporter_ambush_tech =
+{
+  file   = "quest/gtd_cage_teleporter_m.wad"
+  map    = "MAP01"
+
+  theme  = "!hell"
+
+  prob   = 500
+
+  where  = "seeds"
+  shape  = "U"
+
+  deep   = 16
+
+  seed_w = 2
+  seed_h = 2
+
+  bound_z1 = 0
+  
+  kind = "marine"
+
+  x_fit = { 92,100 , 156,164 }
+  y_fit = "top"
+}
+
+PREFABS.Quest_teleporter_ambush_hell =
+{
+  template = "Quest_marine_teleporter_ambush_tech"
+  map      = "MAP02"
+
+  theme    = "hell"
+
+  x_fit    = "frame"
+}
