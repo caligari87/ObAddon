f9d6d5fc6f69f807cabfa9744c4a7c1efa6fc28b
GTD-Carthage <gtdcarthage@yahoo.com>
2020-09-02 11:38:58 +0800

    - Added new options under Prefab Control to enable or disable fabs of specific kinds. Requires further expansion in the future.

--- src/modules/prefab_controller.lua
+++ src/modules/prefab_controller.lua
@@ -18,7 +18,7 @@
 
 PREFAB_CONTROL = { }
 
-PREFAB_CONTROL.CHOICES =
+PREFAB_CONTROL.WALL_CHOICES =
 {
   "fab_default", _("DEFAULT"),
   "fab_some",    _("Some"),
@@ -38,16 +38,77 @@ PREFAB_CONTROL.POINT_CHOICES =
   "fab_heaps",   _("Heaps"),
 }
 
-PREFAB_CONTROL.DETAIL_CHOICES =
+PREFAB_CONTROL.ON_OFF =
 {
   "on",  _("On"),
   "off", _("Off"),
+}
+
+PREFAB_CONTROL.FINE_TUNE_FABS =
+{
+  crushers =
+  {
+    "Joiner_tech_triple_crusher",
+    "Joiner_tech_3x3_crusher",
+    "Joiner_tech_double_mega_crusher",
+    "Hallway_conveyorh_c6",
+    "Hallway_hellcata_i8",
+  }
+
+  gambling =
+  {
+    "Item_dem_gamble_closet1",
+    "Item_dem_gamble_closet2",
+  }
+
+  dexterity =
+  {
+    "Joiner_scionox_minichasm",
+    "Joiner_scionox_minichasm_2",
+  }
+
+  sight_ambushes =
+  {
+    "Cage_auto_open",
+    "Cage_auto_open_smaller",
+    "Cage_teleporter_ambush_tech",
+    "Cage_teleporter_ambush_hell",
+    "Cage_dem_garage_ambush1",
+    "Cage_dem_garage_ambush1CBL",
+    "Cage_dem_garage_ambush1CBR",
+    "Cage_dem_garage_ambush2",
+    "Cage_scionox_guardpost_destroyed",
+  }
 }
 
 function PREFAB_CONTROL.setup(self)
   for name,opt in pairs(self.options) do
     local value = self.options[name].value
     PARAM[name] = value
+  end
+end
+
+function PREFAB_CONTROL.fine_tune_filters()
+  local tab_remove_fabs = {}
+
+  if PARAM.pf_crushers and PARAM.pf_crushers == "off" then
+    table.append(tab_remove_fabs, PREFAB_CONTROL.FINE_TUNE_FABS["crushers"])
+  end
+
+  if PARAM.pf_gamble and PARAM.pf_gamble == "off" then
+    table.append(tab_remove_fabs, PREFAB_CONTROL.FINE_TUNE_FABS["gambling"])
+  end
+
+  if PARAM.pf_dexterity and PARAM.pf_dexterity == "off" then
+    table.append(tab_remove_fabs, PREFAB_CONTROL.FINE_TUNE_FABS["dexterity"])
+  end
+
+  if PARAM.pf_sight_ambushes and PARAM.pf_sight_ambushes == "off" then
+    table.append(tab_remove_fabs, PREFAB_CONTROL.FINE_TUNE_FABS["sight_ambushes"])
+  end
+
+  each fab in tab_remove_fabs do
+    PREFABS[fab] = nil
   end
 end
 
@@ -65,6 +126,7 @@ OB_MODULES["prefab_control"] =
   hooks =
   {
     setup = PREFAB_CONTROL.setup
+    get_levels = PREFAB_CONTROL.fine_tune_filters
   }
 
   options =
@@ -73,10 +135,10 @@ OB_MODULES["prefab_control"] =
     {
       name = "autodetail"
       label=("Auto Detailing")
-      choices=PREFAB_CONTROL.DETAIL_CHOICES
-      tooltip = "Forces Walls to be 'Less' if a map is Extreme-sized, in order to reduce linedef overflow and BSP leakage."
+      choices=PREFAB_CONTROL.ON_OFF
+      tooltip = "Reduces the amount of complex architecture in a map based on its size. Default is on."
       default = "on"
-      priority = 4
+      priority = 102
       gap = 1
     }
 
@@ -87,25 +149,79 @@ OB_MODULES["prefab_control"] =
       choices=PREFAB_CONTROL.POINT_CHOICES
       tooltip = "Decor prefabs are prefabs placed along the floors such as crates, pillars, and other decorative elements which aren't tied to walls. This directly modifies probabilities on a per-room basis, not the density for decor prefabs in any given room.\n\nNote: DEFAULT actually behaves like Mix-It-Up."
       default = "fab_default"
-      priority = 3
+      priority = 101
     }
 
     wall_prob = -- code for this option is currently under revision
     {
       name = "wall_prob"
       label=_("Walls")
-      choices=PREFAB_CONTROL.CHOICES
+      choices=PREFAB_CONTROL.WALL_CHOICES
       tooltip = "Determines the amount plain wall prefabs. What it actually does is greatly increase the probability of Oblige's basic plain wall prefab, rather than reduce the probability of all the prefabs in the library."
       default = "fab_default"
-      priority = 2
+      priority = 100
       gap = 1
     }
+
+    --
+
+    --[[pf_lifts =
+    {
+      name="pf_lifts", label=_("Lifts"), choices=PREFAB_CONTROL.ON_OFF
+      tooltip="Enables or disables lift fabs. Default is on."
+      default="on"
+      priority = 50
+    }]]
+
+    pf_crushers =
+    {
+      name="pf_crushers", label=_("Crushers"), choices=PREFAB_CONTROL.ON_OFF
+      tooltip="Enables or disables fabs with crusing sectors. Default is on."
+      default="on"
+      priority = 49
+    }
+
+    pf_dexterity =
+    {
+      name="pf_dexterity", label=_("Dexterity Fabs"), choices=PREFAB_CONTROL.ON_OFF
+      tooltip="Enables or disables fabs featuring Chasm-ish navigation. Default is on."
+      default="on"
+      priority = 48
+    }
+
+    --[[pf_jump =
+    {
+      name="pf_jump", label=_("Jumps"), choices=PREFAB_CONTROL.ON_OFF
+      tooltip="Enables or disables fabs absolutely requiring jumping to traverse. Default is on."
+      default="on"
+      priority = 47
+    }]]
+
+    pf_gamble =
+    {
+      name="pf_dexterity", label=_("Gambling Fabs"), choices=PREFAB_CONTROL.ON_OFF
+      tooltip="Enables or disables fabs that may lockout a player on items. Default is on."
+      default="on"
+      priority = 46
+    }
+
+    pf_sight_ambushes =
+    {
+      name="pf_sight_ambushes", label=_("Sight Ambush Cages"), choices=PREFAB_CONTROL.ON_OFF
+      tooltip="Enables or disables cages that unleash its monsters when player is in sight. " ..
+      "Default is on."
+      default="on"
+      priority = 45
+      gap = 1
+    }
+
+    --
 
     fab_match_theme =
     {
       name = "fab_match_theme"
       label=("Match Theme")
-      choices=PREFAB_CONTROL.DETAIL_CHOICES
+      choices=PREFAB_CONTROL.ON_OFF
       tooltip = "Ensures that prefabs selected match their intended Theme."
       default = "on"
       priority = 1
