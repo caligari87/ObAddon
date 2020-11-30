8f7f3c5e2da93151689434d071049d67aa0171d2
Tobby GTD-Carthage Ong <gtdcarthage@gmail.com>
2020-11-30 12:37:28 +0800

    - Added some new tech-themed I pieces for vanilla vent hallway set.

---
+++ src/games/doom/fabs/hall/vent_i_tech.lua
@@ -0,0 +1,34 @@
+PREFABS.Hallway_vent_tech_i1 =
+{
+  file   = "hall/vent_i_tech.wad"
+  map    = "MAP01"
+
+  theme  = "!hell"
+  group  = "vent"
+  prob   = 50
+
+  where  = "seeds"
+  shape  = "I"
+
+  sector_1 = {[0] = 2, [1] = 1, [2] = 1, [3] = 1, [21] = 1}
+
+  can_flip = true
+}
+
+PREFABS.Hallway_vent_tech_i2 =
+{
+  template = "Hallway_vent_tech_i1"
+  map = "MAP02"
+}
+
+PREFABS.Hallway_vent_tech_i3 =
+{
+  template = "Hallway_vent_tech_i1"
+  map = "MAP03"
+}
+
+PREFABS.Hallway_vent_tech_i4 =
+{
+  template = "Hallway_vent_tech_i1"
+  map = "MAP04"
+}
