0d3ca1c159fb487d5f265facdb37dd77752405aa
Armaetus <6969122+Armaetus@users.noreply.github.com>
2021-01-11 09:32:54 -0500

    Unfinished work in progress
    More work done here. Gives "Script Error: bad argument #3 to 'format' (number expected, got string)" when going through things in map generation..

--- src/modules/complex_doom.lua
+++ src/modules/complex_doom.lua
@@ -70,17 +70,17 @@ UNFINISHED.MONSTERS =
     level = 1
     prob = 50
     health = 45 -- Average health. All replacement health divided by 4.
-    damage = 5.0 -- Need some sort of average damage due to the replacements.
+    damage = 8.0 -- Need some sort of average damage due to the replacements.
     attack = "missile" -- None of these are instant hit except railgunner's slug, so changed here.
     replaces = "shooter"
     replace_prob = 20
     give = { {ammo="bullet",count=5}, {ammo="grenade", count=1}, {ammo="mine", count=1}, {health="health_flask", count=5}, {ammo="ammo_pack", count=1}, {ammo="cells", count=20} }
-    weap_prefs = { pistol=0.7, shotty=1.2, chain=1.5 } -- Pistol is actually useful on these guys!
+    weap_prefs = { shotty=1.2, chain=1.5 } -- Pistol is actually useful on these guys!
     density = 1.5
     room_size = "any" --small
     disloyal = true
     trap_factor = 0.05
-    infight_damage = 6.0 -- Due to the projectile damage done
+    infight_damage = 15.0 -- Due to the projectile damage done
   }
 
   -- Possible replacements:
@@ -96,7 +96,7 @@ UNFINISHED.MONSTERS =
     level = 1
     prob = 75
     health = 50
-    damage = 8.0
+    damage = 14.0
     attack = "hitscan"
     density = 1.0
     give = { {weapon="shotty"}, {ammo="shell",count=4}, {ammo="bullet",count=5}, {ammo="grenade", count=1}, {ammo="mine", count=1}, {health="health_flask", count=5}, {ammo="ammo_pack", count=1} }
@@ -108,7 +108,7 @@ UNFINISHED.MONSTERS =
     room_size = "any" --small
     disloyal = true
     trap_factor = 2.0
-    infight_damage = 10.0
+    infight_damage = 20.0
   }
 
   -- Possible replacements:
@@ -124,16 +124,17 @@ UNFINISHED.MONSTERS =
     level = 1
     prob = 140
     health = 85
-    damage = 5.0
+    damage = 8.0
     attack = "missile"
     density = 1.0
     give = { {health="health_flask", count=5}, {ammo="armor_shard", count=1} }
     replaces = "demon"
     replace_prob = 25
+    weap_needed = { shotty=true }
     weap_prefs = { shotty=1.5, chain=1.25, super=1.2, plasma=1.2 }
     room_size = "any" --small
     trap_factor = 0.5 --0.3
-    infight_damage = 10.0
+    infight_damage = 12.0
   }
 
   skull =
@@ -141,10 +142,10 @@ UNFINISHED.MONSTERS =
     id = 3006
     r = 16
     h = 56
-    level = 4
+    level = 5
     prob = 25
     health = 100
-    damage = 1.7
+    damage = 6.0
     attack = "melee"
     density = 0.5
     float = true
@@ -153,7 +154,7 @@ UNFINISHED.MONSTERS =
     disloyal = true
     trap_factor = 0.35 --0.2
     cage_factor = 0
-    infight_damage = 2.1
+    infight_damage = 8.0
   }
 
   -- Possible replacements:
@@ -166,16 +167,17 @@ UNFINISHED.MONSTERS =
     id = 3002
     r = 30
     h = 56
-    level = 3
+    level = 4
     prob = 50
     health = 260
-    damage = 7.0
+    damage = 15.0
     attack = "melee"
     density = 0.85
     weap_min_damage = 40
+    weap_needed = { shotty=true }
     weap_prefs = { super=1.75, shotty=1.35, chain=1.5, plasma=1.2, launch=0.3 }
     room_size = "any"
-    infight_damage = 15
+    infight_damage = 40
   }
 
 -- Same as Demon.
@@ -184,40 +186,41 @@ UNFINISHED.MONSTERS =
     id = 58
     r = 30
     h = 56
-    level = 3
+    level = 4
     replaces = "demon"
     replace_prob = 35
     crazy_prob = 25
-    health = 220
-    damage = 7.0
+    health = 260
+    damage = 15.0
     attack = "melee"
     density = 0.5
     invis = true
     outdoor_factor = 3.0
     weap_min_damage = 40
+    weap_needed = { shotty=true }
     weap_prefs = { super=1.75, shotty=1.35, chain=1.5, plasma=1.2, launch=0.3 }
     species = "demon"
     room_size = "any"
     trap_factor = 0.3
-    infight_damage = 15
+    infight_damage = 40
   }
 
--- Shows up sooner and increased chance to replace
--- the Pain Elemental.
   caco =
   {
     id = 3005
     r = 31
     h = 56
-    level = 3
+    level = 5
     prob = 30
-    health = 400
-    damage = 8.0
+    health = 600
+    damage = 12.0
     attack = "missile"
     density = 0.6
     weap_min_damage = 40
     float = true
+    weap_needed = { chain=true }
     weap_prefs = { launch=1.25, super=1.75, chain=1.2, shotty=0.7, plasma=1.2 }
+    weap_min_damage = 40
     replaces = "pain"
     replace_prob = 20
     room_size = "any" --large
@@ -228,41 +231,39 @@ UNFINISHED.MONSTERS =
 
   ---| BOSSES |---
 
--- Shows up sooner.
   baron =
   {
     id = 3003
     r = 24
     h = 64
-    level = 5
+    level = 7
     boss_type = "minor"
     boss_prob = 50
     prob = 6.4
     crazy_prob = 20
     weap_needed = { launch=true }
     weap_prefs = { launch=1.75, super=1.5, plasma=1.75, bfg=1.5 }
-    health = 1000
-    damage = 15.0
+    health = 1250
+    damage = 35.0
     attack = "missile"
     density = 0.3
     weap_min_damage = 88
     room_size = "any" --medium
-    infight_damage = 70
+    infight_damage = 120
   }
 
--- Shows up sooner.
   Cyberdemon =
   {
     id = 16
     r = 40
     h = 110
-    level = 6 --8
+    level = 9
     boss_type = "tough"
     boss_prob = 50
     prob = 1.6
     crazy_prob = 10
     health = 4000
-    damage = 150
+    damage = 300
     attack = "missile"
     density = 0.1
     weap_needed = { bfg=true }
@@ -274,22 +275,21 @@ UNFINISHED.MONSTERS =
     boss_replacement = "baron"
   }
 
--- Shows up sooner and increased chance to be used
--- in maps. Added cage_factor to prevent any chance
+-- Added cage_factor to prevent any chance
 -- of placement in cages.
   Spiderdemon =
   {
     id = 7
     r = 128
     h = 100
-    level = 6
+    level = 8
     boss_type = "tough"
     boss_prob = 15
     boss_limit = 1 -- because they infight
     prob = 2.0
     crazy_prob = 10
     health = 3000
-    damage = 150
+    damage = 250
     attack = "hitscan"
     density = 0.1
     cage_factor = 0
@@ -317,7 +317,7 @@ UNFINISHED.MONSTERS =
     level = 1.6
     prob = 60
     health = 80
-    damage = 12.0
+    damage = 15.0
     attack = "hitscan"
     give = { {weapon="chain"}, {ammo="bullet",count=10}, {ammo="bullet",count=5}, {ammo="grenade", count=1}, {ammo="mine", count=1}, {ammo="health_flask", count=5}, {ammo="ammo_pack", count=1} }
     weap_needed = { chain=true }
@@ -363,10 +363,10 @@ UNFINISHED.MONSTERS =
     id = 69
     r = 24
     h = 64
-    level = 4
+    level = 5
     prob = 26
     health = 500
-    damage = 15.0
+    damage = 20.0
     attack = "missile"
     weap_min_damage = 50
     weap_prefs = { launch=1.75, super=1.5, plasma=1.33 }
@@ -379,16 +379,16 @@ UNFINISHED.MONSTERS =
   }
 
 -- Shows up sooner, increased replacement for Arachnotron
--- and can show up outside more.
+-- and can show up outside more. Also has a floating replacement!
   mancubus =
   {
     id = 67
     r = 48
     h = 64
-    level = 5
+    level = 6
     prob = 20
     health = 600
-    damage = 12.0
+    damage = 20.0
     attack = "missile"
     weap_prefs = { launch=1.5, super=1.5, plasma=1.5, chain=1.2 }
     density = 0.32
@@ -411,7 +411,7 @@ UNFINISHED.MONSTERS =
     level = 4.5
     prob = 12
     health = 500
-    damage = 15.0
+    damage = 25.0
     attack = "missile"
     weap_min_damage = 60
     weap_prefs = { launch=1.5, super=1.5, plasma=1.5, chain=1.2 }
@@ -426,6 +426,9 @@ UNFINISHED.MONSTERS =
 -- Shows up sooner and increased number that can be
 -- placed in one room. Can replace boss monster for
 -- the Baron.
+
+-- NOTES:
+-- Summoner replacement is very nasty, so prob has been dropped.
   vile =
   {
     id = 64
@@ -433,20 +436,20 @@ UNFINISHED.MONSTERS =
     h = 56
     level = 8
     boss_type = "nasty"
-    boss_prob = 50
+    boss_prob = 20
     boss_limit = 1 -- Vile replacements are pretty nasty, hence limited to 1
-    prob = 5
-    crazy_prob = 15
-    health = 700
+    prob = 2
+    crazy_prob = 5
+    health = 850
     damage = 40
     attack = "hitscan"
-    density = 0.12
+    density = 0.1
     room_size = "medium"
-    weap_needed = { super=true }
+    weap_needed = { bfg=true }
     weap_prefs = { launch=3.0, super=1.5, plasma=2.0, bfg=2.5 }
     weap_min_damage = 120
     nasty = true
-    infight_damage = 70
+    infight_damage = 100
     boss_replacement = "baron"
   }
 
@@ -499,7 +502,7 @@ UNFINISHED.MONSTERS =
   }
 }
 
-COMPLEX_DOOM.WEAPONS =
+UNFINISHED.WEAPONS =
 {
   fist =
   {
@@ -628,7 +631,7 @@ COMPLEX_DOOM.WEAPONS =
 }
 
 -- Need some changes here too!
-COMPLEX_DOOM.PICKUPS =
+UNFINISHED.PICKUPS =
 {
   -- HEALTH --
 
@@ -639,28 +642,6 @@ COMPLEX_DOOM.PICKUPS =
     add_prob = 20
     cluster = { 3,7 }
     give = { {health=2} }
-  }
-
-  stimpack =
-  {
-    id = 2011
-    kind = "health"
-    add_prob = 60
-    cluster = { 2,5 }
-    give = { {health=10} }
-  }
-
-  medikit =
-  {
-    id = 2012
-    kind = "health"
-    rank = 2
-    add_prob = 120
-    closet_prob = 20
-    secret_prob = 5
-    storage_prob = 80
-    storage_qty  = 2
-    give = { {health=25} }
   }
 
   -- ARMOR --
@@ -674,87 +655,6 @@ COMPLEX_DOOM.PICKUPS =
     give = { {health=2} }
   }
 
-  -- AMMO --
-
-  bullets =
-  {
-    id = 2007
-    kind = "ammo"
-    add_prob = 10
-    cluster = { 2,5 }
-    give = { {ammo="bullet",count=10} }
-  }
-
-  bullet_box =
-  {
-    id = 2048
-    kind = "ammo"
-    rank = 2
-    add_prob = 35
-    give = { {ammo="bullet",count=50} }
-  }
-
-  shells =
-  {
-    id = 2008
-    kind = "ammo"
-    add_prob = 20
-    cluster = { 2,5 }
-    give = { {ammo="shell",count=4} }
-  }
-
-  shell_box =
-  {
-    id = 2049
-    kind = "ammo"
-    rank = 2
-    add_prob = 40
-    give = { {ammo="shell",count=20} }
-  }
-
-  rocket =
-  {
-    id = 2010
-    kind = "ammo"
-    add_prob = 6
-    cluster = { 4,7 }
-    give = { {ammo="rocket",count=1} }
-  }
-
-  rocket_box =
-  {
-    id = 2046
-    kind = "ammo"
-    rank = 2
-    add_prob = 25
-    closet_prob = 20
-    secret_prob = 5
-    storage_prob = 20
-    storage_qty  = 3
-    give = { {ammo="rocket",count=5} }
-  }
-
-  cells =
-  {
-    id = 2047
-    kind = "ammo"
-    add_prob = 20
-    closet_prob = 20
-    cluster = { 2,5 }
-    give = { {ammo="cell",count=20} }
-  }
-
-  cell_pack =
-  {
-    id = 17
-    kind = "ammo"
-    rank = 2
-    add_prob = 30
-    secret_prob = 5
-    storage_prob = 40
-    storage_qty  = 2
-    give = { {ammo="cell",count=100} }
-  }
 
  -- Complex Doom items, never placed but used for letting Oblige know
  -- that monsters can drop more than one kind of item.
@@ -817,78 +717,6 @@ COMPLEX_DOOM.PICKUPS =
 --------------------------------------------------------------------
 
 
-COMPLEX_DOOM.NICE_ITEMS =
-{
-  -- HEALTH / ARMOR --
-
-  green_armor =
-  {
-    id = 2018
-    kind = "armor"
-    add_prob = 50
-    start_prob = 60
-    crazy_prob = 5
-    closet_prob = 10
-    give = { {health=30} }
-  }
-
-  blue_armor =
-  {
-    id = 2019
-    kind = "armor"
-    add_prob = 5
-    start_prob = 10
-    secret_prob = 60
-    give = { {health=80} }
-  }
-
-  soul =
-  {
-    id = 2013
-    kind = "health"
-    add_prob = 5
-    start_prob = 0
-    closet_prob = 2
-    secret_prob = 40
-    give = { {health=150} }
-  }
-
-  -- WEAPONS --
-
-  saw =
-  {
-    id = 2005
-    kind = "other"  -- really a weapon
-    add_prob = 7
-    secret_prob = 10
-    once_only = true
-  }
-
-  -- POWERUP --
-
-  invis =
-  {
-    id = 2024
-    kind = "powerup"
-    add_prob = 79
-    start_prob = 5
-    closet_prob = 15
-    time_limit = 100
-  }
-
-  --
-  -- NOTES:
-  --
-  -- The All-map is for secrets only, hence has no add_prob.
-  --
-  -- Radiation suit has no probs (and hence is never added) since it
-  -- needs special logic, e.g. when creating areas of nukage or lava
-  -- which the player is forced to cross.
-  --
-}
-
-
-
 function COMPLEX_DOOM.setup(self)
   if not PARAM.doom2_weapons then
     GAME.MONSTERS["gunner"] = nil
@@ -918,12 +746,12 @@ UNFINISHED["COMPLEX_DOOM"] =
 
   side = "left"
   priority = 61
-  game = { doom1=1, doom2=1, heretic=0, hexen=0 } -- Dunno if it has support for Heretic/Hexen
+  game = { doom1=1, doom2=1 } -- Dunno if it has support for Heretic/Hexen
 
   tooltip = "Alters to fit the difficulty that Complex Doom provides. Do not use with 'Harder Enemy Setup' addon, will conflict."
 
   -- Zandronum *SHOULD* work with v27, otherwise will remove it.
-  engine = { zdoom=1, gzdoom=1, skulltag=1, limit=0, heretic=0, hexen=0 }
+  engine = { zdoom=1, gzdoom=1, skulltag=1 }
 
   tables =
   {
