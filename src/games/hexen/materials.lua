164699ef419525ecaf44765d2638d6769ca64827
Armaetus <6969122+Armaetus@users.noreply.github.com>
2021-01-18 12:01:57 -0500

    Hexen placeholder files
    Base file parameters commented out so Oblige doesn't give an error on launch.

--- src/games/hexen/materials.lua
+++ src/games/hexen/materials.lua
@@ -1,11 +1,12 @@
 ------------------------------------------------------------------------
---  BASE FILE for HEXEN
+--  MATERIALS for HEXEN
 ------------------------------------------------------------------------
 --
 --  Oblige Level Maker
 --
 --  Copyright (C) 2006-2015 Andrew Apted
 --  Copyright (C) 2011-2012 Jared Blackburn
+--  Copyright (C) 2021 Armaetus
 --
 --  This program is free software; you can redistribute it and/or
 --  modify it under the terms of the GNU General Public License
@@ -19,45 +20,7 @@
 --
 ------------------------------------------------------------------------
 
-HEXEN = { }
-
-
-------------------------------------------------------------
---gui.import("params")
-
---gui.import("entities")
---gui.import("monsters")
---gui.import("pickups")
---gui.import("weapons")
-
---gui.import("materials")
---gui.import("themes")
---gui.import("levels")
---gui.import("resources")
-
-------------------------------------------------------------
-
-UNFINISHED["hexen"] =
-{
-  label = _("Hexen")
-  priority = 34
-
-  -- Hexen map format is a minor variation on the DOOM format
-  format = "doom"
-  sub_format = "hexen"
-
-  game_dir = "hexen"
-  iwad_name = "hexen.wad"
-
-  tables =
-  {
-    HEXEN
-  }
-
-  hooks =
-  {
-    get_levels = HEXEN.get_levels
-    all_done   = HEXEN.all_done
-  }
-}
-
+-- This is so Oblige can load without it bitching about the file missing
+-- on startup.
+--
+-- HEXEN DOES NOT WORK SO DO NOT BOTHER...YET
