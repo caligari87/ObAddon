0b044dcbfebd4fed6961c66ab1362ab4a7477723
Dasho Danger <dashodanger@outlook.com>
2021-02-02 14:29:44 +0430

    Heretic prefab additions/fixes

--- src/games/heretic/fabs/hall/vent_k.lua
+++ src/games/heretic/fabs/hall/vent_k.lua
@@ -1,13 +1,55 @@
 --
 -- vent piece : locked terminators
 --
+
+PREFABS.Hallway_vent_locked_yellow =
+{
+  file   = "hall/vent_k.wad"
+  map    = "MAP01"
+
+  kind   = "terminator"
+  group  = "vent"
+  key    = "k_yellow"
+
+  prob   = 50
+
+  where  = "seeds"
+  shape  = "I"
+
+  deep   = 16
+
+}
+
+
+PREFABS.Hallway_vent_locked_green =
+{
+  template = "Hallway_vent_locked_yellow"
+
+  key    = "k_green"
+
+  line_34     = 33
+  tex_DRIPWALL = "GRNBLOK1"
+}
+
+
+PREFABS.Hallway_vent_locked_blue =
+{
+  template = "Hallway_vent_locked_yellow"
+
+  key    = "k_blue"
+
+  line_34     = 32
+  tex_DRIPWALL = "BLUEFRAG"
+}
+
+
 ----------------------------------------------------------------
+
 
 PREFABS.Hallway_vent_barred =
 {
   file   = "hall/vent_k.wad"
   map    = "MAP03"
-  theme  = "!tech"
 
   kind   = "terminator"
   group  = "vent"
