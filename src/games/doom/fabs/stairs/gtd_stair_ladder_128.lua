2bc64bc0af762c8a8ec06d90e0cfda4f15cd8bb5
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-02-27 09:24:23 +0800

    - Removed the stretchy stairs fab.

--- src/games/doom/fabs/stairs/gtd_stair_ladder_128.lua
+++ src/games/doom/fabs/stairs/gtd_stair_ladder_128.lua
@@ -10,8 +10,10 @@ PREFABS.Ladder_128_rustic =
   shape  = "I"
 
   seed_w = 1
+  seed_h = 1
 
-  x_fit  = {48, 80}
+  x_fit = "frame"
+  y_fit = "bottom"
 
   bound_z1 = 0
 
@@ -24,7 +26,6 @@ PREFABS.Ladder_128_rustic =
 PREFABS.Ladder_128_techy =
 {
   template = "Ladder_128_rustic"
-  prob = 3
 
   theme = "!hell"
 
@@ -46,62 +47,22 @@ PREFABS.Ladder_128_woody =
   flat_CEIL5_2 = "FLAT5_1"
 }
 
-PREFABS.Ladder_128_rustic_2x =
-{
-  template = "Ladder_128_rustic"
-
-  map = "MAP02"
-
-  seed_h = 2
-}
-
-PREFABS.Ladder_128_techy_2x =
-{
-  template = "Ladder_128_rustic"
-
-  map = "MAP02"
-  prob = 3
-
-  seed_h = 2
-
-  theme = "!hell"
-
-  tex_STEPLAD1 = "STEP4"
-  tex_METAL = "SILVER1"
-  tex_SUPPORT3 = {SUPPORT2=50, LITEBLU1=50, SUPPORT3=50}
-  flat_CEIL5_2 = "FLAT19"
-}
-
-PREFABS.Ladder_128_woody_2x =
-{
-  template = "Ladder_128_rustic"
-
-  map = "MAP02"
-
-  seed_h = 2
-
-  theme = "!tech"
-
-  tex_STEPLAD1 = {STEP2=50, STEP6=50, STEP3=50}
-  tex_METAL = "WOOD8"
-  tex_SUPPORT3 = "WOOD8"
-  flat_CEIL5_2 = "FLAT5_1"
-}
-
 PREFABS.Ladder_thin =
 {
   file = "stairs/gtd_stair_ladder_128.wad"
-  map = "MAP03"
+  map = "MAP02"
 
-  prob = 3
+  prob = 9
   style = "steepness"
 
   where  = "seeds"
   shape  = "I"
 
   seed_w = 1
+  seed_h = 1
 
   x_fit = "frame"
+  y_fit = "bottom"
 
   bound_z1 = 0
 
@@ -115,12 +76,4 @@ PREFABS.Ladder_thin =
     STEP3 = 25
     STEPTOP = 50
   }
-}
-
-PREFABS.Ladder_thin_2x =
-{
-  template = "Ladder_thin"
-  map = "MAP04"
-
-  seed_h = 2
 }
